55ea188faac9202009895e953707890297058d6e
Ali Turfah <aturfah@umich.edu>
2018-01-30 13:56:55 -0500

    Add elo calculations

---
+++ battle_engine/elo.py
@@ -0,0 +1,10 @@
+""" Functions to compute new elo values for a player """
+
+def expected(player1, player2):
+    """ Expected score of player1 vs player2 given elo rankings """
+    return 1 / (1 + 10 ** ((player2.elo - player1.elo) / 400))
+
+def new_elo(player1, player2, outcome, k=32):
+    """ Calculate new elo score given outcome of match"""
+    exp = expected(player1, player2)
+    return player1.elo + k * (outcome - exp)
