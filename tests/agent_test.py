ebeec91ee90d3b17cbfc4236cdd232e81c8463a3
Ali Turfah <aturfah@umich.edu>
2018-02-01 12:17:07 -0500

    Comments :D

--- tests/agent_test.py
+++ tests/agent_test.py
@@ -4,8 +4,12 @@ from agent.base_agent import Base_Agent
 def testWinLoss():
     """ Test calculation of win/loss ratio"""
     ba1 = Base_Agent()
+
+    # Wins with no losses has undefined w/l ratio
     ba1.num_wins = 50
     assert(ba1.win_loss_ratio() == None) 
+
+    # Losses makes the calculation valid
     ba1.num_losses = 10
     assert(ba1.win_loss_ratio() == 5) 
 
