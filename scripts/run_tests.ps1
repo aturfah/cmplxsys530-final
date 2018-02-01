e78fae99dfca1606e352044a49457c9a99960804
Ali Turfah <aturfah@umich.edu>
2018-02-01 12:13:33 -0500

    Comment out test script

--- scripts/run_tests.ps1
+++ scripts/run_tests.ps1
@@ -1,8 +1,11 @@
 echo "Running tests..."
 
-# 
+# Run Agent Tests
 echo "Running agent_test.py"
 python -m tests.agent_test
+echo "Completed!"
 
+# Run tests on Elo calculations
 echo "Running elo_test.py"
 python -m tests.elo_test
+echo "Completed!"
