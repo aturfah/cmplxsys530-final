af219fb75297ac004b406d3f5cfdcc03ad2b1850
Ali Turfah <aturfah@umich.edu>
2018-02-01 12:15:52 -0500

    Rewrite agent test to functions

--- tests/agent_test.py
+++ tests/agent_test.py
@@ -1,12 +1,12 @@
 """ Test functionality of base agent """
 from agent.base_agent import Base_Agent
 
+def testWinLoss():
+    """ Test calculation of win/loss ratio"""
+    ba1 = Base_Agent()
+    ba1.num_wins = 50
+    assert(ba1.win_loss_ratio() == None) 
+    ba1.num_losses = 10
+    assert(ba1.win_loss_ratio() == 5) 
 
-ba1 = Base_Agent()
-# ba1.hello()
-
-ba1.num_wins = 50
-assert(ba1.win_loss_ratio() == None) 
-ba1.num_losses = 10
-assert(ba1.win_loss_ratio() == 5) 
-
+testWinLoss()
