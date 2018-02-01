26ea6c3458b840929ee52c348f77659f1e1e9009
Ali Turfah <aturfah@umich.edu>
2018-02-01 12:29:50 -0500

    Formatting

--- tests/coinflip_test.py
+++ tests/coinflip_test.py
@@ -2,3 +2,23 @@
 from agent.base_agent import Base_Agent
 from battle_engine.coinflip import CoinFlipEngine
 
+
+def test_init():
+    player1 = Base_Agent()
+    player2 = Base_Agent()
+
+    # Try to initialize CoinFlipEngine with negative probability
+    try:
+        cfe = CoinFlipEngine(player1, player2, -1)
+        assert(False)
+    except AttributeError:
+        pass
+
+    # Try to initialize CoinFlipEngine with probability > 1
+    try:
+        cfe = CoinFlipEngine(player1, player2, 50)
+        assert(False)
+    except AttributeError:
+        pass
+
+test_init()
--- tests/elo_test.py
+++ tests/elo_test.py
@@ -5,6 +5,7 @@ from ladder.elo import elo
 
 # Using wikipedia's example from https://en.wikipedia.org/wiki/Elo_rating_system
 
+
 def test_drop():
     """ Test that ratings drop when appropriate """
     playerA = Base_Agent()
@@ -12,8 +13,9 @@ def test_drop():
     playerA.elo = 1619
     playerB.elo = 1609
 
-    playerA.elo = elo(playerA, playerB, 0) 
+    playerA.elo = elo(playerA, playerB, 0)
     assert(playerA.elo == 1602)
+
 
 def test_floor():
     """ Tests that ratings do not go below 1000 """
@@ -23,6 +25,7 @@ def test_floor():
     playerA.elo = elo(playerA, playerB, 0)
     assert(playerA.elo == 1000)
 
+
 def test_increase():
     """ Tests that ratings increase when appropriate """
     playerA = Base_Agent()
@@ -30,9 +33,9 @@ def test_increase():
     playerA.elo = 1619
     playerB.elo = 1609
 
-    playerB.elo = elo(playerB, playerA, 1) 
-    print(playerB.elo)
+    playerB.elo = elo(playerB, playerA, 1)
     assert(playerB.elo == 1625)
+
 
 test_increase()
 test_drop()
