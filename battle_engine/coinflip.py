317ffd28d75f370203f0167f26b24b00fef164ad
Ali Turfah <aturfah@umich.edu>
2018-02-01 12:39:46 -0500

    Add method to run coinflip game

--- battle_engine/coinflip.py
+++ battle_engine/coinflip.py
@@ -1,5 +1,6 @@
 """ Coin Flip Engine, random chance of winning """
 from ladder.elo import elo
+from numpy.random import uniform
 
 class CoinFlipEngine:
     def __init__(self, player1, player2, prob_win = 0.5):
@@ -17,7 +18,22 @@ class CoinFlipEngine:
         self.p2 = player2
         self.prob_win = prob_win
 
+    def results(winner, loser):
+        """ Update values for winner and loser """
+        new_winner_elo = elo(winner, loser, 1)
+        new_loser_elo = elo(winner, loser, 0)
+        winner.elo = new_winner_elo
+        winner.num_wins += 1
+        loser.elo = new_loser_elo
+        loser.num_losses += 1
+
     def run(self):
         """ Run the game, in this case draw from U(0,1)"""
-        
-        pass
+        draw = uniform()
+
+        if draw < self.prob_win:
+            # Player1 wins
+            results(self.p1, self.p2)
+        else:
+            # Player2 wins
+            results(self.p2, self.p1)
