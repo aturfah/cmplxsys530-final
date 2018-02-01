61b6925c13017b5d91db0b382193175930af9261
Ali Turfah <aturfah@umich.edu>
2018-02-01 11:27:48 -0500

    Remove prints from agent_test.py

--- tests/agent_test.py
+++ tests/agent_test.py
@@ -7,8 +7,6 @@ ba1.hello()
 
 ba1.num_wins = 50
 assert(ba1.win_loss_ratio() == None) 
-print(ba1.win_loss_ratio())
 ba1.num_losses = 10
 assert(ba1.win_loss_ratio() == 5) 
-print(ba1.win_loss_ratio())
 
