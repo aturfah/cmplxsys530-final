fd8014cdf34193b1418810bd0c4df75eb6356ad1
Ali Turfah <aturfah@umich.edu>
2018-02-04 18:43:48 -0500

    Add test for run_game

--- tests/ladder_test.py
+++ tests/ladder_test.py
@@ -2,15 +2,14 @@
 
 from agent.base_agent import Base_Agent
 from ladder.ladder import Ladder
-
+from battle_engine.coinflip import CoinFlipEngine
 
 def test_add():
     """ Basic test for ladder add_player method """
     lad = Ladder()
     ba1 = Base_Agent()
     ba2 = Base_Agent()
-    ba1.hello()
-    ba2.hello()
+
     lad.add_player(ba1)
     lad.add_player(ba2)
 
@@ -34,6 +33,7 @@ def test_no_duplicates():
 
 
 def test_match():
+    """ Test that match functions properly """
     lad = Ladder()
     ba1 = Base_Agent()
     ba2 = Base_Agent()
@@ -47,6 +47,37 @@ def test_match():
     assert(len(lad.player_pool) == 0)
     assert(lad.num_turns == 1)
 
+def test_run_game():
+    """ Test run_game functions properly """
+    lad = Ladder()
+    ba1 = Base_Agent()
+    ba2 = Base_Agent()
+
+    lad.add_player(ba1)
+    lad.add_player(ba2)
+
+    cfe = CoinFlipEngine()
+
+    lad.run_game(cfe)
+
+    players = lad.get_players()
+
+    player1 = players[0]
+    player2 = players[1]
+
+    # Only one elo value changes
+    assert((player1.elo > 1000 and player2.elo == 1000) or
+         (player1.elo == 1000 and player2.elo > 1000))
+
+    # Someone won the game
+    assert((player1.num_wins == 0 and player2.num_wins == 1) or
+         (player1.num_wins == 1 and player2.num_wins == 0))
+
+    # Someone lost the game
+    assert((player1.num_losses == 0 and player2.num_losses == 1) or
+         (player1.num_losses == 1 and player2.num_losses == 0))
+
 test_add()
 test_no_duplicates()
 test_match()
+test_run_game()
