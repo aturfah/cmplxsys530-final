22cb670782ca5b3b9c9c173937cf8f06dedd56f2
Ali Turfah <aturfah@umich.edu>
2018-02-04 17:49:25 -0500

    Ladder Test (and fix ladder __init__)

--- ladder/ladder.py
+++ ladder/ladder.py
@@ -2,29 +2,30 @@
 
 from numpy.random import randint
 
+
 class Ladder:
-    def ___init__(self):
+    def __init__(self):
         """ Initialize a ladder """
-        self.player_pool = []
+        self.player_pool = set()
         self.num_turns = 0
 
     def add_player(self, player):
         """ Add a player to the waiting pool """
-        player_pool.append({
-            "player": player,
-            "num_turns": num_turns
-        })
+        self.player_pool.add((
+            player,
+            self.num_turns
+        ))
 
     def match():
         """ Return a pair of players to play """
         # Select a random player
-        player_ind = randint(low = 0, high = length(self.player_pool))
-        player = player_pool[player_ind]["player"]
+        player_ind = randint(low=0, high=length(self.player_pool))
+        player = player_pool[player_ind][0]
         del player_pool[player_ind]
 
         # Select that player's opponent (for now its random)
-        opponent_ind = randint(low = 0, high = length(self.player_pool))
-        opponent = player_pool[opponent_ind]["player"]
+        opponent_ind = randint(low=0, high=length(self.player_pool))
+        opponent = player_pool[opponent_ind][0]
         del player_pool[opponent_ind]
 
         self.num_turns += 1
--- tests/ladder_test.py
+++ tests/ladder_test.py
@@ -1,5 +1,14 @@
 """ Script to test functioning of ladder """
 
 from agent.base_agent import Base_Agent
-from ladder.ladder import ladder
+from ladder.ladder import Ladder
 
+
+def test_add():
+    lad = Ladder()
+    ba1 = Base_Agent()
+    ba2 = Base_Agent()
+    lad.add_player(ba1)
+    lad.add_player(ba2)
+
+test_add()
