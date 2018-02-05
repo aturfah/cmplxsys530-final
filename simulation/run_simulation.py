bdb96b6b3805e147de8654e29cd93d1a164bb2cc
Ali Turfah <aturfah@umich.edu>
2018-02-04 19:28:25 -0500

    Remove misc print statements

--- simulation/run_simulation.py
+++ simulation/run_simulation.py
@@ -10,7 +10,6 @@ from ladder.ladder import Ladder
 @click.option('--num_players', default=10, help='Number of agents')
 @click.option('--game_choice', default=0, help='Which game to play. Options are:\n\t [0] coin_flip')
 def run(num_runs, num_players, game_choice):
-    print("Hello World!")
     game = None
     if game_choice == 0:
         game = CoinFlipEngine()
