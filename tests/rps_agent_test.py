ad29a4097bf254dc9940cc047d3e76301cd7951b
Ali Turfah <aturfah@umich.edu>
2018-02-04 20:03:52 -0500

    Finihs basic test

--- tests/rps_agent_test.py
+++ tests/rps_agent_test.py
@@ -7,7 +7,8 @@ def basic_test():
     """ Test basic functionality """
     rps1 = RPS_Agent()
 
-    rps1.hello()
-    print(rps1.win_loss_ratio())
+    assert(rps1.elo == 1000)
+    assert(rps1.win_loss_ratio() == None)
+
 
 basic_test()
