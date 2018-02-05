2adf1ce25ab7688fb2b3434a909195e87403d182
Ali Turfah <aturfah@umich.edu>
2018-02-05 12:13:49 -0500

    Pycodestyle formatting

--- tests/rps_agent_test.py
+++ tests/rps_agent_test.py
@@ -14,37 +14,38 @@ def basic_test():
 
     error = False
     try:
-        RPS_Agent(strategy_in = 'VOMIT')
+        RPS_Agent(strategy_in='VOMIT')
     except ValueError:
         # We should be here
         error = True
-    assert(error) # Invalid strategy input
+    assert(error)  # Invalid strategy input
 
     error = False
     try:
-        RPS_Agent(strategy_in = [1, 1, 1])
+        RPS_Agent(strategy_in=[1, 1, 1])
     except ValueError:
         error = True
-    assert(error) # Strategies cannot sum to 1
+    assert(error)  # Strategies cannot sum to 1
 
     error = False
     try:
-        RPS_Agent(strategy_in = [-1, 1, 1])
+        RPS_Agent(strategy_in=[-1, 1, 1])
     except ValueError:
         error = True
-    assert(error) # No negative strategies
+    assert(error)  # No negative strategies
 
     assert(rps1.elo == 1000)
-    assert(rps1.win_loss_ratio() == None)
+    assert(rps1.win_loss_ratio() is None)
+
 
 def test_make_move():
     """ Test make_move method """
-    rps_rock = RPS_Agent(strategy_in = [1, 0, 0])
-    rps_paper = RPS_Agent(strategy_in = [0, 1, 0])
-    rps_scissors = RPS_Agent(strategy_in = [0, 0, 1])
+    rps_rock = RPS_Agent(strategy_in=[1, 0, 0])
+    rps_paper = RPS_Agent(strategy_in=[0, 1, 0])
+    rps_scissors = RPS_Agent(strategy_in=[0, 0, 1])
     rps_random = RPS_Agent()
 
-    assert(rps_random.make_move() in [0,1,2])
+    assert(rps_random.make_move() in [0, 1, 2])
     assert(rps_rock.make_move() == 0)
     assert(rps_paper.make_move() == 1)
     assert(rps_scissors.make_move() == 2)
