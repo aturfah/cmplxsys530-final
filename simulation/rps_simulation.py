8f65792ce12bf2ae3dfab79969afc738467c5dd9
Ali Turfah <aturfah@umich.edu>
2018-02-05 12:06:01 -0500

    Pycodestyle formatting

--- simulation/rps_simulation.py
+++ simulation/rps_simulation.py
@@ -8,7 +8,9 @@ from ladder.ladder import Ladder
 from stats.calc import calculate_avg_elo
 from stats.plot import plot_group_ratings
 
-def run(num_runs, num_players, proportions, suppress_print, suppress_graph, data_delay):
+
+def run(num_runs, num_players, proportions,
+        suppress_print, suppress_graph, data_delay):
     game = RPSEngine()
     lad = Ladder()
     ratings = {}
@@ -25,22 +27,22 @@ def run(num_runs, num_players, proportions, suppress_print, suppress_graph, data
 
     for rock_ind in range(num_rock):
         agent_id = 'rock_{}'.format(rock_ind)
-        player = RPS_Agent(id_in = agent_id, strategy_in = 'rock')
+        player = RPS_Agent(id_in=agent_id, strategy_in='rock')
         lad.add_player(player)
 
     for paper_ind in range(num_paper):
         agent_id = 'paper_{}'.format(paper_ind)
-        player = RPS_Agent(id_in = agent_id, strategy_in = 'paper')
+        player = RPS_Agent(id_in=agent_id, strategy_in='paper')
         lad.add_player(player)
 
     for sciss_ind in range(num_scissors):
         agent_id = 'scissors_{}'.format(sciss_ind)
-        player = RPS_Agent(id_in = agent_id, strategy_in = 'scissors')
+        player = RPS_Agent(id_in=agent_id, strategy_in='scissors')
         lad.add_player(player)
 
     for mixed_ind in range(num_mixed):
         agent_id = 'mixed_{}'.format(mixed_ind)
-        player = RPS_Agent(id_in = agent_id)
+        player = RPS_Agent(id_in=agent_id)
         lad.add_player(player)
 
     for game_ind in range(num_runs):
@@ -52,7 +54,7 @@ def run(num_runs, num_players, proportions, suppress_print, suppress_graph, data
                 if group not in ratings:
                     ratings[group] = []
                 ratings[group].append(current_stats[group])
-    
+
     players = lad.get_players(sort=True)
 
     if not suppress_print:
