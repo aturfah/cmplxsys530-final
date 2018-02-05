0ae855db2553a2c27e2132955e3b9dc0affc387c
Ali Turfah <aturfah@umich.edu>
2018-02-05 12:05:04 -0500

    Pycodestyle formatting

--- simulation/cfe_simulation.py
+++ simulation/cfe_simulation.py
@@ -1,11 +1,13 @@
 """ Script to run a ladder simulation for CoinFlipEngine """
 
+
 from battle_engine.coinflip import CoinFlipEngine
 from agent.base_agent import Base_Agent
 from ladder.ladder import Ladder
 
+
 def run(num_runs, num_players, suppress_print):
-    game = CoinFlipEngine()    
+    game = CoinFlipEngine()
     lad = Ladder()
 
     for _ in range(num_players):
@@ -14,7 +16,7 @@ def run(num_runs, num_players, suppress_print):
 
     for _ in range(num_runs):
         lad.run_game(game)
-    
+
     players = lad.get_players(sort=True)
 
     if not suppress_print:
