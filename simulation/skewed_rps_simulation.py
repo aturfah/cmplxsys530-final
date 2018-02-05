9ad55777bbc76bba8c55edab8ad68e6e2e351563
Ali Turfah <aturfah@umich.edu>
2018-02-04 23:19:24 -0500

    Add calculations and plotting for skewed rps

--- simulation/skewed_rps_simulation.py
+++ simulation/skewed_rps_simulation.py
@@ -5,10 +5,13 @@ from math import ceil
 from battle_engine.rockpaperscissors import RPSEngine
 from agent.rps_agent import RPS_Agent
 from ladder.ladder import Ladder
+from stats.calc import calculate_avg_elo
+from stats.plot import plot_group_ratings
 
 def run(num_runs, num_players, proportions):
     game = RPSEngine()
     lad = Ladder()
+    ratings = {}
 
     prop_rock = float(proportions[0])
     prop_paper = float(proportions[1])
@@ -34,10 +37,19 @@ def run(num_runs, num_players, proportions):
         player = RPS_Agent(id_in = agent_id, strategy_in = 'scissors')
         lad.add_player(player)
 
-    for _ in range(num_runs):
+    for game_ind in range(num_runs):
         lad.run_game(game)
+        if game_ind % 10 == 0:
+            # Calculate the statistics every 10 values
+            current_stats = calculate_avg_elo(lad)
+            for group in current_stats:
+                if group not in ratings:
+                    ratings[group] = []
+                ratings[group].append(current_stats[group])
     
     players = lad.get_players(sort=True)
 
     for player in players:
         player.print_info()
+
+    plot_group_ratings(ratings)
