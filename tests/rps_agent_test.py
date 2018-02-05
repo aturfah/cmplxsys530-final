502527396f942c8839a0db21d3419309a39da01f
Ali Turfah <aturfah@umich.edu>
2018-02-04 20:04:39 -0500

    Add test for make_move

--- tests/rps_agent_test.py
+++ tests/rps_agent_test.py
@@ -10,5 +10,16 @@ def basic_test():
     assert(rps1.elo == 1000)
     assert(rps1.win_loss_ratio() == None)
 
+def test_make_move():
+    """ Test make_move method """
+    rps_rock = RPS_Agent(strategy = [1, 0, 0])
+    rps_paper = RPS_Agent(strategy = [0, 1, 0])
+    rps_scissors = RPS_Agent(strategy = [0, 0, 1])
+
+    assert(rps_rock.make_move() == 0)
+    assert(rps_paper.make_move() == 1)
+    assert(rps_scissors.make_move() == 2)
+
 
 basic_test()
+test_make_move()
