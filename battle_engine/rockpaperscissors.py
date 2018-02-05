e012c2adf41d3fca44dc35ab6a92c47d4f4b1129
Ali Turfah <aturfah@umich.edu>
2018-02-04 20:46:15 -0500

    Add logic for RPS game

--- battle_engine/rockpaperscissors.py
+++ battle_engine/rockpaperscissors.py
@@ -1,4 +1,16 @@
 """ Engine implementation for Rock, Paper, Scissors """
+from numpy.random import uniform
+
 
 class RPS_Engine:
-    pass
+    def run(self, player1, player2):
+        """ Run a game of Rock, Paper, Scissors """
+        p1_move = player1.make_move()
+        p2_move = player2.make_move()
+
+        if p1_move == p2_move:
+            return uniform() > 0.5
+        elif p1_move - p2_move == 1:
+            return 1
+        else:
+            return 0
