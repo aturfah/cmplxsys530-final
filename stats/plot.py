8711ed6f95d44dac47cf69069b0402634499a069
Ali Turfah <aturfah@umich.edu>
2018-02-04 23:27:32 -0500

    Add plotting

--- stats/plot.py
+++ stats/plot.py
@@ -1,4 +1,14 @@
 """ Plotting some neat statistics from a ladder """
 
+import matplotlib.pyplot as plt
+
 def plot_group_ratings(data):
-    pass
+    print("plotting data...")
+    legend_info = []
+
+    for group in data:
+        plt.plot(data[group])
+        legend_info.append(group)
+    
+    plt.legend(legend_info, loc='upper left')
+    plt.show()
