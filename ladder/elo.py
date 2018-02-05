2649a48987dc17a674d866b9aa43092437e5d3eb
Ali Turfah <aturfah@umich.edu>
2018-02-05 12:03:38 -0500

    Pycodestyle styling

--- ladder/elo.py
+++ ladder/elo.py
@@ -1,19 +1,21 @@
 """ Functions to compute new elo values for a player """
 from math import floor
 
+
 def expected(player1, player2):
     """
     Expected score of player1 vs player2 given elo rankings
-    
+
     :param player1: Agent for whom the score is being calculated
     :param player2: Agent against whom player1 played
     """
     return 1 / (1 + 10 ** ((player2.elo - player1.elo) / 400))
 
+
 def elo(player1, player2, outcome, k=32):
     """
     Calculate new elo score given outcome of match
-    
+
     :param player1: Agent for whom the score is being calculated
     :param player2: Agent against whom player1 played
     :param outcome: Win (1) or loss(0) [there are no draws]
