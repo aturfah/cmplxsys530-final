a9cce1835092e8f7027847649ead5c4b80e08f2e
Ali Turfah <aturfah@umich.edu>
2018-02-05 12:28:59 -0500

    Pydocstyle rps_engine

--- battle_engine/rockpaperscissors.py
+++ battle_engine/rockpaperscissors.py
@@ -1,10 +1,12 @@
-""" Engine implementation for Rock, Paper, Scissors """
+"""Engine implementation for Rock, Paper, Scissors."""
 from numpy.random import uniform
 
 
 class RPSEngine:
+    """Engine to run a game of rock, paper, scissors."""
+
     def run(self, player1, player2):
-        """ Run a game of Rock, Paper, Scissors """
+        """Run a game of Rock, Paper, Scissors."""
         p1_move = player1.make_move()
         p2_move = player2.make_move()
 
