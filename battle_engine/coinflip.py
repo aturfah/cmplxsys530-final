7284a758cb86a72922d04417128f2a1cf92d2813
Ali Turfah <aturfah@umich.edu>
2018-02-05 12:26:32 -0500

    Pydocstyle coinflip

--- battle_engine/coinflip.py
+++ battle_engine/coinflip.py
@@ -1,11 +1,13 @@
-""" Coin Flip Engine, random chance of winning """
+"""Coin Flip Engine, random chance of winning."""
 from numpy.random import uniform
 
 
 class CoinFlipEngine:
+    """Engine to run coin flip game."""
+
     def __init__(self, prob_win=0.5):
         """
-        Initialize a random Coin Flip Engine, winner decided by a coin flip
+        Initialize a random Coin Flip Engine, winner decided by a coin flip.
 
         :param player1: Agent that will participate in the game
         :param player2: The other agent that will participate in the game
@@ -17,7 +19,7 @@ class CoinFlipEngine:
         self.prob_win = prob_win
 
     def run(self, player1, player2):
-        """ Run the game, in this case draw from U(0,1)"""
+        """Run the game, in this case draw from U(0,1)."""
         draw = uniform()
 
         if draw < self.prob_win:
