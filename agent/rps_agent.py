d98a14ccb4f6dba56531d9a87ee2c55a42b2573c
Ali Turfah <aturfah@umich.edu>
2018-02-04 20:39:23 -0500

    Add logic to check for negative probability

--- agent/rps_agent.py
+++ agent/rps_agent.py
@@ -2,16 +2,33 @@
 from agent.base_agent import Base_Agent
 from numpy.random import uniform
 
+strategies = {
+    'rock': [1,0,0],
+    'paper': [0,1,0],
+    'scissors': [0,0,1],
+    'uniform': [1/3, 1/3, 1/3]
+}
+
 class RPS_Agent(Base_Agent):
-    def __init__(self, strategy = [1/3, 1/3, 1/3]):
+    def __init__(self, strategy_in = 'uniform'):
         """ 
         Create a Rock/Paper/Scissors player who plays Rock, Paper, or Scissors
         according to probabilities in strategy.
 
-        :param strategy: Vector of probabilities to play any of Rock, Paper, Scissors respectively
+        :param strategy: Either a string corresponding to one of the strategies,
+             or a vector of probabilities to play Rock, Paper, Scissors respectively
         """
+        if isinstance(strategy_in, list):
+            strategy = strategy_in
+        elif strategy_in in strategies:
+            strategy = strategies[strategy_in]
+        else:
+            raise ValueError('Invalid strategy')
+
         if len(strategy) is not 3:
             raise ValueError('Strategy vector must be of length 3')
+        if any(strategy_prob < 0 for strategy_prob in strategy):
+            raise ValueError('Strategy probabilities cannot be less than 0')
         if not(abs(sum(strategy) - 1) < 0.0000000000000000000001):
             # Not arbitrarily close to 1
             # TODO: Fix this its nasty
