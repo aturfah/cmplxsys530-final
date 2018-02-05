1537fbe6271b9b1f7919c09fdbbeb6c3394a8d24
Ali Turfah <aturfah@umich.edu>
2018-02-05 12:10:51 -0500

    Pycodestyle formatting

--- tests/base_agent_test.py
+++ tests/base_agent_test.py
@@ -1,17 +1,18 @@
 """ Test functionality of base agent """
 from agent.base_agent import Base_Agent
 
+
 def testWinLoss():
     """ Test calculation of win/loss ratio"""
     ba1 = Base_Agent()
 
     # Wins with no losses has undefined w/l ratio
     ba1.num_wins = 50
-    assert(ba1.win_loss_ratio() == None) 
+    assert(ba1.win_loss_ratio() is None)
 
     # Losses makes the calculation valid
     ba1.num_losses = 10
-    assert(ba1.win_loss_ratio() == 5) 
+    assert(ba1.win_loss_ratio() == 5)
 
     # num_wins + num_losses = total_games
     assert(ba1.total_games() == 60)
@@ -25,8 +26,9 @@ def testMakeMove():
         ba1.make_move()
     except NotImplementedError:
         return
-    
+
     assert(False)
+
 
 testWinLoss()
 testMakeMove()
