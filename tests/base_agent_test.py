52b208874e2e5c4edea9b92b5d8cb4d6b0ac07d3
Ali Turfah <aturfah@umich.edu>
2018-02-06 15:23:21 -0500

    Pylint/Docstyle for base_agent_test

--- tests/base_agent_test.py
+++ tests/base_agent_test.py
@@ -1,25 +1,25 @@
-""" Test functionality of base agent """
+"""Test functionality of base agent."""
 from agent.base_agent import BaseAgent
 
 
-def testWinLoss():
-    """ Test calculation of win/loss ratio"""
+def test_win_loss():
+    """Test calculation of win/loss ratio."""
     ba1 = BaseAgent()
 
     # Wins with no losses has undefined w/l ratio
     ba1.num_wins = 50
-    assert(ba1.win_loss_ratio() is None)
+    assert ba1.win_loss_ratio() is None
 
     # Losses makes the calculation valid
     ba1.num_losses = 10
-    assert(ba1.win_loss_ratio() == 5)
+    assert ba1.win_loss_ratio() == 5
 
     # num_wins + num_losses = total_games
-    assert(ba1.total_games() == 60)
+    assert ba1.total_games() == 60
 
 
-def testMakeMove():
-    """ Tests that make_move throws an error """
+def test_make_move():
+    """Tests that make_move throws an error."""
     ba1 = BaseAgent()
 
     try:
@@ -27,8 +27,8 @@ def testMakeMove():
     except NotImplementedError:
         return
 
-    assert(False)
+    assert False
 
 
-testWinLoss()
-testMakeMove()
+test_win_loss()
+test_make_move()
