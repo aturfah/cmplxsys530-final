ae8f0578db7248856637f28a897c628bd5acac47
Ali Turfah <aturfah@umich.edu>
2018-02-06 15:43:31 -0500

    Pylint/Docstyling for rps agent test

--- tests/rps_agent_test.py
+++ tests/rps_agent_test.py
@@ -1,16 +1,16 @@
-""" Unit tests for Rock/Paper/Scissors agent """
+"""Unit tests for Rock/Paper/Scissors agent."""
 
 from agent.rps_agent import RPSAgent
 
 
 def basic_test():
-    """ Test basic functionality """
+    """Test basic functionality."""
     # Test constructor
     rps1 = RPSAgent()
     rps_rock = RPSAgent('rock')
     rps_rock2 = RPSAgent([1, 0, 0])
 
-    assert(rps_rock.strategy == rps_rock2.strategy)
+    assert rps_rock.strategy == rps_rock2.strategy
 
     error = False
     try:
@@ -18,37 +18,37 @@ def basic_test():
     except ValueError:
         # We should be here
         error = True
-    assert(error)  # Invalid strategy input
+    assert error  # Invalid strategy input
 
     error = False
     try:
         RPSAgent(strategy_in=[1, 1, 1])
     except ValueError:
         error = True
-    assert(error)  # Strategies cannot sum to 1
+    assert error  # Strategies cannot sum to 1
 
     error = False
     try:
         RPSAgent(strategy_in=[-1, 1, 1])
     except ValueError:
         error = True
-    assert(error)  # No negative strategies
+    assert error  # No negative strategies
 
-    assert(rps1.elo == 1000)
-    assert(rps1.win_loss_ratio() is None)
+    assert rps1.elo == 1000
+    assert rps1.win_loss_ratio() is None
 
 
 def test_make_move():
-    """ Test make_move method """
+    """Test make_move method."""
     rps_rock = RPSAgent(strategy_in=[1, 0, 0])
     rps_paper = RPSAgent(strategy_in=[0, 1, 0])
     rps_scissors = RPSAgent(strategy_in=[0, 0, 1])
     rps_random = RPSAgent()
 
-    assert(rps_random.make_move() in [0, 1, 2])
-    assert(rps_rock.make_move() == 0)
-    assert(rps_paper.make_move() == 1)
-    assert(rps_scissors.make_move() == 2)
+    assert rps_random.make_move() in [0, 1, 2]
+    assert rps_rock.make_move() == 0
+    assert rps_paper.make_move() == 1
+    assert rps_scissors.make_move() == 2
 
 
 basic_test()
