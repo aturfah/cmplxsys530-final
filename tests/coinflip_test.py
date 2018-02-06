0bed5aa49e4b6b2fd02ccb347bf8c177dfad1dfa
Ali Turfah <aturfah@umich.edu>
2018-02-06 15:26:00 -0500

    Pydocstyle/Pylint coinflip test

--- tests/coinflip_test.py
+++ tests/coinflip_test.py
@@ -1,39 +1,37 @@
-""" Test functionality of Coinflip Engine """
+"""Test functionality of Coinflip Engine."""
 from agent.base_agent import BaseAgent
 from battle_engine.coinflip import CoinFlipEngine
 
 
 def test_init():
-    player1 = BaseAgent()
-    player2 = BaseAgent()
-
+    """Test CFE engine init method."""
     # Try to initialize CoinFlipEngine with negative probability
     try:
         cfe = CoinFlipEngine(prob_win=-1)
-        assert(False)
+        assert False
     except AttributeError:
         pass
 
     # Try to initialize CoinFlipEngine with probability > 1
     try:
         cfe = CoinFlipEngine(prob_win=50)
-        assert(False)
+        assert False
     except AttributeError:
         pass
 
 
 def test_run():
-    """ Run single iteration of game """
+    """Run single iteration of game."""
     player1 = BaseAgent()
     player2 = BaseAgent()
 
     cfe_win = CoinFlipEngine(prob_win=1)
     outcome = cfe_win.run(player1, player2)
-    assert(outcome == 1)
+    assert outcome == 1
 
     cfe_loss = CoinFlipEngine(prob_win=0)
     outcome = cfe_loss.run(player1, player2)
-    assert(outcome == 0)
+    assert outcome == 0
 
 
 test_init()
