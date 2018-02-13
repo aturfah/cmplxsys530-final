128fba4639f27203a4df0bea1e7d4f2600d132e0
Ali Turfah <aturfah@umich.edu>
2018-02-13 12:18:46 -0500

    Update test with providing the line

--- tests/log_writer_test.py
+++ tests/log_writer_test.py
@@ -5,7 +5,7 @@ from log_manager.log_writer import LogWriter
 def test_basic():
     """Basic test for LogWriter class."""
     lw1 = LogWriter(prefix="test")
-    lw1.write_line()
+    lw1.write_line(['pew', 'foo', 'bar'])
 
 
 test_basic()
