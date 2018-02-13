5c4433347149961c4ea3843a16ef3e15a6a77f45
Ali Turfah <aturfah@umich.edu>
2018-02-13 10:18:11 -0500

    Add generate_filename func

--- log_manager/log_writer.py
+++ log_manager/log_writer.py
@@ -1,14 +1,38 @@
 """Log Manager Class."""
-
+from datetime import datetime
 
 class LogWriter():
     """Class for class that generates log files."""
 
-    def __init__(self):
+    def __init__(self, prefix = None):
+        """Initialize LogWriter for a simulation"""
+        filename = generate_filename(prefix)
+        print(filename)
         print("Init logwriter")
     
     def __del__(self):
+        """Delete LogWriter"""
         print("Delete logwriter")
 
     def write_line(self):
+        """Write line to this output"""
         pass
+
+
+def generate_filename(prefix = None):
+    if not prefix:
+        prefix_str = ""
+    else:
+        prefix_str = prefix + "_"
+
+    now = datetime.now()
+    day = "%02d" % now.day
+    month = "%02d" % now.month
+    year = now.year
+    hour = "%02d" % now.hour
+    minute = "%02d" % now.minute
+    second = "%02d" % now.second
+
+    filename = "{}{}/{}/{}_{}-{}-{}".format(prefix_str, year, month, day, hour, minute, second)
+    
+    return filename
