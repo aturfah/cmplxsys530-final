599702eddd37a82f0e4128b6b0d17f1141b5485d
Ali Turfah <aturfah@umich.edu>
2018-02-13 15:21:25 -0500

    Fix parameters in elo

--- ladder/elo.py
+++ ladder/elo.py
@@ -6,8 +6,10 @@ def expected(player1, player2):
     """
     Calculate expected score of player1 vs player2 given elo rankings.
 
-    :param player1: Agent for whom the score is being calculated
-    :param player2: Agent against whom player1 played
+    :param player1: BaseAgent
+        Agent for whom the score is being calculated
+    :param player2: BaseAgent
+        Agent against whom player1 played
     """
     return 1 / (1 + 10 ** ((player2.elo - player1.elo) / 400))
 
@@ -16,10 +18,14 @@ def elo(player1, player2, outcome, k=32):
     """
     Calculate new elo score given outcome of match.
 
-    :param player1: Agent for whom the score is being calculated
-    :param player2: Agent against whom player1 played
-    :param outcome: Win (1) or loss(0) [there are no draws]
-    :param k: The k-factor for the Elo score
+    :param player1: BaseAgent
+        Agent for whom the score is being calculated
+    :param player2: BaseAgent
+        Agent against whom player1 played
+    :param outcome: int, one of 0/1
+        Win (1) or loss(0) [there are no draws]
+    :param k: int
+        The k-factor for the Elo score
     """
     exp = expected(player1, player2)
     new_elo = floor(player1.elo + k * (outcome - exp))
