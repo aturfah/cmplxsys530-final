db6cdf28ba9d44d6c0c039497f63b7cd8ae9c4b8
Ali Turfah <aturfah@umich.edu>
2018-02-13 12:25:38 -0500

    Update test to provide a dict

--- tests/log_writer_test.py
+++ tests/log_writer_test.py
@@ -4,8 +4,14 @@ from log_manager.log_writer import LogWriter
 
 def test_basic():
     """Basic test for LogWriter class."""
-    lw1 = LogWriter(prefix="test")
-    lw1.write_line(['pew', 'foo', 'bar'])
+    header = ["test1", "test2", "test3"]
+    lw1 = LogWriter(header=header, prefix="test")
+    
+    dict_to_write = {}
+    dict_to_write["test1"] = "pew"
+    dict_to_write["test2"] = "foo"
+    dict_to_write["test3"] = "bar"
+    lw1.write_line(dict_to_write)
 
 
 test_basic()
