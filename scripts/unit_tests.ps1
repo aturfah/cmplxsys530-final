ab0d03f0e61302f6e88d8343d95ddfc0b91fc9ca
Ali Turfah <aturfah@umich.edu>
2018-02-14 15:30:45 -0500

    Break out tests into folders

--- scripts/unit_tests.ps1
+++ scripts/unit_tests.ps1
@@ -2,31 +2,31 @@ Write-Output "Running tests..."
 
 # Run Agent Tests
 Write-Output "Running agent_test.py"
-python -m tests.base_agent_test
+python -m tests.agent_tests.base_agent_test
 Write-Output "Completed!"
 
 # Run tests on Elo calculations
 Write-Output "Running elo_test.py"
-python -m tests.elo_test
+python -m tests.misc_tests.elo_test
 Write-Output "Completed!"
 
 # Run tests on Basic Coin Flip Engine
 Write-Output "Running coinflip_test.py"
-python -m tests.coinflip_test
+python -m tests.engine_tests.coinflip_test
 Write-Output "Completed!"
 
 Write-Output "Running base_ladder_test.py"
-python -m tests.base_ladder_test
+python -m tests.ladder_tests.base_ladder_test
 Write-Output "Completed!"
 
 Write-Output "Running random_ladder_test.py"
-python -m tests.random_ladder_test
+python -m tests.ladder_tests.random_ladder_test
 Write-Output "Completed!"
 
 Write-Output "Running weighted_ladder_test.py"
-python -m tests.weighted_ladder_test
+python -m tests.ladder_tests.weighted_ladder_test
 Write-Output "Completed!"
 
 Write-Output "Running rps_agent_test.py"
-python -m tests.rps_agent_test
+python -m tests.agent_tests.rps_agent_test
 Write-Output "Completed!"
