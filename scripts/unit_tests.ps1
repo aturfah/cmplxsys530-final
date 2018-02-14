67f91c6c3b48863fd118ec38b30ad850624d2a5c
Ali Turfah <aturfah@umich.edu>
2018-02-14 15:31:27 -0500

    Rearrange order of tests

--- scripts/unit_tests.ps1
+++ scripts/unit_tests.ps1
@@ -1,13 +1,12 @@
 Write-Output "Running tests..."
 
 # Run Agent Tests
-Write-Output "Running agent_test.py"
+Write-Output "Running base_agent_test.py"
 python -m tests.agent_tests.base_agent_test
 Write-Output "Completed!"
 
-# Run tests on Elo calculations
-Write-Output "Running elo_test.py"
-python -m tests.misc_tests.elo_test
+Write-Output "Running rps_agent_test.py"
+python -m tests.agent_tests.rps_agent_test
 Write-Output "Completed!"
 
 # Run tests on Basic Coin Flip Engine
@@ -27,6 +26,7 @@ Write-Output "Running weighted_ladder_test.py"
 python -m tests.ladder_tests.weighted_ladder_test
 Write-Output "Completed!"
 
-Write-Output "Running rps_agent_test.py"
-python -m tests.agent_tests.rps_agent_test
+# Run tests on Elo calculations
+Write-Output "Running elo_test.py"
+python -m tests.misc_tests.elo_test
 Write-Output "Completed!"
