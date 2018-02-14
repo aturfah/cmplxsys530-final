1988b2baaf31c8cd08065d81ed5845955c90c5b9
Ali Turfah <aturfah@umich.edu>
2018-02-14 15:14:45 -0500

    Add random ladder

---
+++ ladder/random_ladder.py
@@ -0,0 +1,33 @@
+"""Ladder that randomly pairs two agents"""
+
+from numpy.random import rand
+from ladder.base_ladder import BaseLadder
+
+class WeightedLadder(BaseLadder):
+    """Ladder that matches players by Elo ranking."""
+
+    def __init__(self, game=None, K_in=32):
+        """
+        Initialize a ladder for a specific game.
+
+        :param game: GameEngine
+            Game to be played on this ladder
+        :param K_in: int
+            K value to be used for calculating elo changes
+            on this ladder
+        """
+        super().__init__(game=game, K_in=K_in)
+
+    def match_func(self, player1, player2_pair):
+        """
+        Return random value as a match
+
+        Since players will be sorted this random value, it is
+        equivalent to randomly choosing an opponent
+
+        :param player1: BaseAgent
+            The player who is being matched
+        :param player2: (BaseAgent, int)
+            The candidate player & turns waiting pair for a  match
+        """
+        return rand()
