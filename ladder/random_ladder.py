37a1ad44cc1228d5b3caa5d6529b86c9b0f7e6d0
Ali Turfah <aturfah@umich.edu>
2018-02-14 23:51:52 -0500

    Fix incorrect comment

--- ladder/random_ladder.py
+++ ladder/random_ladder.py
@@ -5,7 +5,7 @@ from ladder.base_ladder import BaseLadder
 
 
 class RandomLadder(BaseLadder):
-    """Ladder that matches players by Elo ranking."""
+    """Ladder that matches players randomly."""
 
     def __init__(self, game=None, K_in=32):
         """
@@ -24,7 +24,7 @@ class RandomLadder(BaseLadder):
         Return random value as a match weighting.
 
         Since players will be sorted this random value, it is
-        equivalent to randomly choosing an opponent
+        equivalent to randomly choosing an opponent.
 
         :param player1: BaseAgent
             The player who is being matched
