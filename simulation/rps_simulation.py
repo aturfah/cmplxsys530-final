9b3dfb2eadccb57ab4cdcd6e7c89d95a9cd34538
Ali Turfah <aturfah@umich.edu>
2018-02-15 09:14:05 -0500

    Add init_type_log_writer to __init__

--- simulation/rps_simulation.py
+++ simulation/rps_simulation.py
@@ -22,6 +22,7 @@ class RPSSimulation(BaseSimulation):
         self.data_delay = kwargs["data_delay"]
 
         self.add_agents()
+        self.init_type_log_writer()
 
     def add_agents(self):
         """Add agents in specified proportions to ladder."""
