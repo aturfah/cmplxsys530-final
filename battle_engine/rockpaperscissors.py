c2200cde2ba4f5d6f6ff53d5bc852c92c0cfdbe0
Ali Turfah <aturfah@umich.edu>
2018-02-18 11:11:13 -0500

    Remove bias (why was that even there)

--- battle_engine/rockpaperscissors.py
+++ battle_engine/rockpaperscissors.py
@@ -5,14 +5,9 @@ from numpy.random import uniform
 class RPSEngine:
     """Engine to run a game of rock, paper, scissors."""
 
-    def __init__(self, bias=0.5):
-        """
-        Init method for this class.
-
-        :param bias: float
-            How biased this class is in tiebreaking.
-        """
-        self.bias = bias
+    def __init__(self):
+        """Init method for this class."""
+        pass
 
     def run(self, player1, player2):
         """
@@ -31,7 +26,7 @@ class RPSEngine:
 
         if p1_move == p2_move:
             # Same move, call it with a coinflip
-            return int(uniform() > self.bias)
+            return int(uniform() > 0.5)
         elif (p1_move - p2_move) == 1 or (p1_move - p2_move) == -2:
             # Player1 wins (Paper vs Rock or
             #   Scissors vs Paper or Rock vs Scissors)
