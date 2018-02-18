d0cd08920edaf37635707fd7fd3e7b691e8ad0e0
Ali Turfah <aturfah@umich.edu>
2018-02-18 11:40:14 -0500

    Fix styling issues ;__;

--- tests/engine_tests/rockpaperscissors_test.py
+++ tests/engine_tests/rockpaperscissors_test.py
@@ -1,9 +1,11 @@
-"""Test for RPS Engine"""
+"""Test for RPS Engine."""
 
 from battle_engine.rockpaperscissors import RPSEngine
 from agent.rps_agent import RPSAgent
 
+
 def test_run_game():
+    """Test RPS Game executes properly."""
     rps_engine = RPSEngine(bias=1)
     rps_paper = RPSAgent(strategy_in="paper")
     rps_rock1 = RPSAgent(strategy_in="rock")
@@ -12,5 +14,6 @@ def test_run_game():
     assert rps_engine.run(rps_paper, rps_rock1) == 1
     assert rps_engine.run(rps_rock1, rps_paper) == 0
     assert rps_engine.run(rps_rock1, rps_rock2) == 1
+
 
 test_run_game()
