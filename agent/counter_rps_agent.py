811d6d53aa543ca8553e5ec3ec5f6deb15c35b68
Ali Turfah <aturfah@umich.edu>
2018-02-19 15:56:06 -0500

    Add updated make_move method

--- agent/counter_rps_agent.py
+++ agent/counter_rps_agent.py
@@ -2,18 +2,27 @@
 
 from agent.rps_agent import RPSAgent
 
+
 class CounterRPSAgent(RPSAgent):
     """
     Class for Counter RPS Agent.
-    
+
     Strategy is as follows:
         Turn1: Play random
         Turn2->n: Play what would beat opponent.
     """
+
     def __init__(self, id_in=None):
         """Init method."""
         super().__init__(id_in=id_in)
         self.reset_state()
-    
+
     def reset_state(self):
+        """Reset state once game is finished."""
         self.last_move = None
+
+    def make_move(self):
+        """Counter opponent's last move"""
+        if self.last_move is None:
+            super().make_move()
+        return (self.last_move + 1) % 3
