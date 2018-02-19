4b712e0665e9f407d6545991c172a67232fcd21e
Ali Turfah <aturfah@umich.edu>
2018-02-19 14:54:45 -0500

    Add initialize game_state method

--- battle_engine/multiturn_rps.py
+++ battle_engine/multiturn_rps.py
@@ -1,6 +1,17 @@
 """Engine for Best N/M RPS."""
 
+import numpy as np
+
 class MTRPSEngine:
     """Multiturn RPS Engine class."""
-    def __init__(self):
-        pass
+    def __init__(self, num_games=3):
+        """Initialize empty game_state"""
+        self.num_games = num_games
+
+        self.reset_game_state()
+
+
+    def reset_game_state(self):
+        self.game_state = np.zeros(self.num_games, dtype=bool)
+
+
