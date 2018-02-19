d314139fa63aa2a95e632ddc65242e1ee1261d38
Ali Turfah <aturfah@umich.edu>
2018-02-19 16:00:21 -0500

    Add test for updated make_move

--- tests/agent_tests/counter_rps_agent_test.py
+++ tests/agent_tests/counter_rps_agent_test.py
@@ -7,4 +7,25 @@ def test_init():
     CounterRPSAgent()
 
 
+def test_make_move():
+    c_player = CounterRPSAgent()
+
+    # Makes random move
+    assert c_player.make_move() in [0, 1, 2]
+
+    # Counters opponent's move
+    opp_move_rock = 0
+    c_player.last_move = opp_move_rock
+    assert c_player.make_move() == 1
+
+    opp_move_paper = 1
+    c_player.last_move = opp_move_paper
+    assert c_player.make_move() == 2
+
+    opp_move_scissors = 0
+    c_player.last_move = opp_move_scissors
+    assert c_player.make_move() == 0
+
+
 test_init()
+test_make_move()
