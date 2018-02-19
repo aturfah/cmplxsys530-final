7d07e6e27347f714489d8010c8fd8a99908789a3
Ali Turfah <aturfah@umich.edu>
2018-02-19 15:28:10 -0500

    Move stuff around in test

--- tests/engine_tests/multiturn_rps_test.py
+++ tests/engine_tests/multiturn_rps_test.py
@@ -4,11 +4,8 @@ from agent.rps_agent import RPSAgent
 from battle_engine.multiturn_rps import MTRPSEngine
 
 def test_init():
-    mtrps = MTRPSEngine()
-    rps_player1 = RPSAgent(strategy_in="rock")
-    rps_player2 = RPSAgent(strategy_in="paper")
+    MTRPSEngine()
 
-    mtrps.run(rps_player1, rps_player2)
 
 
 test_init()
