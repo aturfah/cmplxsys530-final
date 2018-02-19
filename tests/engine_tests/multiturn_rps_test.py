0c76a4a9dea3f41894bbd9fd42894bc90cfe5812
Ali Turfah <aturfah@umich.edu>
2018-02-19 15:29:46 -0500

    Add parameter validation test

--- tests/engine_tests/multiturn_rps_test.py
+++ tests/engine_tests/multiturn_rps_test.py
@@ -3,9 +3,30 @@
 from agent.rps_agent import RPSAgent
 from battle_engine.multiturn_rps import MTRPSEngine
 
+
 def test_init():
     MTRPSEngine()
 
 
+def test_param_validation():
+    try:
+        MTRPSEngine(num_games=2)
+        assert False
+    except AttributeError:
+        pass
+
+    try:
+        MTRPSEngine(num_games=-1)
+        assert False
+    except AttributeError:
+        pass
+
+    try:
+        MTRPSEngine(num_games=0)
+        assert False
+    except AttributeError:
+        pass
+
 
 test_init()
+test_param_validation()
