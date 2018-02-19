dafb66f54e7c986b74c63616767f1d025bb44190
Ali Turfah <aturfah@umich.edu>
2018-02-19 14:42:52 -0500

    Add comments

--- visualize_results.py
+++ visualize_results.py
@@ -16,16 +16,22 @@ def run(prefix):
     """Visualize the data from a LogReader."""
     filenames = None
     if prefix is None:
+        # Hide TK Window
         root = Tk()
         root.withdraw()
         root.update()
         filenames = askopenfilenames()
         root.destroy()
+
+        # Cancelled file load
         if not filenames:
             raise RuntimeError("You must specify a prefix or select files.")
 
+    # Initialize LogReader with files/prefix
     log_reader = LogReader(prefix=prefix, filenames=filenames)
     log_reader.read_data()
+
+    # All data is numeric in this case, fix later
     log_reader.to_numeric(log_reader.data_keys)
 
     plot.plot_log_reader_data(log_reader)
