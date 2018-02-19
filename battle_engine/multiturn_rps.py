3e6bcbf44c36fdecd78a188e2c0c4d7e9807f3c1
Ali Turfah <aturfah@umich.edu>
2018-02-19 15:22:14 -0500

    Finish win_condition_met code and also the loop

--- battle_engine/multiturn_rps.py
+++ battle_engine/multiturn_rps.py
@@ -21,33 +21,61 @@ class MTRPSEngine:
 
     def reset_game_state(self):
         """Reset game state to all zeros."""
-        self.game_state = np.zeros(self.num_games)
+        self.game_state = {}
+        self.game_state[0] = 0
+        self.game_state[1] = 0
+        self.game_state[2] = 0
 
     def run(self, player1, player2):
         """Run a set of games."""
         self.reset_game_state()
+        outcome = None
         for index in range(self.num_games):
             p1_move = player1.make_move()
             p2_move = player2.make_move()
 
             if p1_move == p2_move:
                 # Draw
-                self.game_state[index] = 0
+                self.game_state[0] += 1
             elif (p1_move - p2_move) == 1 or (p1_move - p2_move) == -2:
                 # player1 wins (Paper vs Rock or
                 #   Scissors vs Paper or Rock vs Scissors)
-                self.game_state[index] = 1
+                self.game_state[1] += 1
             else:
                 # player2 wins
-                self.game_state[index] = 2
-            self.win_condition_met()
-        print(self.game_state)
+                self.game_state[2] += 1
+
+            outcome = self.win_condition_met()
+            if not outcome["draw"]:
+                print("Player{} won in {} turns".format(
+                    outcome["winner"], index))
+                break
+
+        if not outcome["draw"]:
+            return outcome["winner"]
+
+        return int(uniform() < 0.5)
 
     def win_condition_met(self):
         """
-        Assess whehter or not condition for winning met and return winner.
-        
+        Assess whether or not condition for winning met and return winner.
+
         If number of games necessary for victory met, return the winner.
         """
-        unique, counts = np.unique(self.game_state, return_counts=True)
-        print(unique, counts)
+        p1_wins = self.game_state[1]
+        p2_wins = self.game_state[2]
+        wins_needed = int(self.num_games/2)+1
+
+        # Initialize results
+        result = {}
+        result["draw"] = True
+        result["winner"] = None
+
+        if p1_wins >= wins_needed:
+            result["draw"] = False
+            result["winner"] = 1
+        elif p2_wins >= wins_needed:
+            result["draw"] = False
+            result["winner"] = 2
+
+        return result
