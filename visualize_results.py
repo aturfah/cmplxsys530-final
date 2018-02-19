9b7ab4562ee6b373f57bde6423d79fb7a98f3797
Ali Turfah <aturfah@umich.edu>
2018-02-19 10:57:40 -0500

    Move code around

--- visualize_results.py
+++ visualize_results.py
@@ -1,11 +1,13 @@
 """Methods to visualize the results of simulations."""
 import click
 import matplotlib.pyplot as plt
+import numpy as np
 from matplotlib.widgets import CheckButtons
 
 
 from log_manager.log_reader import LogReader
 from stats import plot
+from stats import calc
 
 
 @click.command()
@@ -18,21 +20,19 @@ def run(prefix):
 
     log_reader = LogReader(prefix=prefix)
     log_reader.read_data()
+    data_range = calc.calculate_data_range(log_reader, log_reader.header)
 
-    # print(log_reader.data)
     _, axis = plt.subplots()
     plt.subplots_adjust(right=0.8)
+
     legend_info = []
     graph_dict = {}
-
     for group in log_reader.data:
-        line_i, = axis.plot(log_reader.data[group])
         legend_info.append(group)
+        line_i, = axis.plot(log_reader.data[group])
         graph_dict[group] = line_i
 
-    change_axis = plt.axes([0.85, 0.65, 0.13, 0.2])
-    change_buttons = CheckButtons(
-        change_axis, legend_info, [True]*len(legend_info))
+    plt.legend(legend_info, loc='upper left')
 
     def new_data(event):
         """PEW."""
@@ -40,8 +40,11 @@ def run(prefix):
         line.set_visible(not line.get_visible())
         plt.draw()
 
-    change_buttons.on_clicked(new_data)
+    change_axis = plt.axes([0.85, 0.65, 0.13, 0.2])
+    change_buttons = CheckButtons(
+        change_axis, legend_info, [True]*len(legend_info))
 
+    change_buttons.on_clicked(new_data)
     plt.show()
 
 
