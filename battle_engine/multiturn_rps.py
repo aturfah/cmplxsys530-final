44af601deae3dbcc0f1b32ef01bb9c3319796fa1
Ali Turfah <aturfah@umich.edu>
2018-02-19 15:03:25 -0500

    Add run() code

--- battle_engine/multiturn_rps.py
+++ battle_engine/multiturn_rps.py
@@ -2,12 +2,14 @@
 
 import numpy as np
 
+
 class MTRPSEngine:
     """Multiturn RPS Engine class."""
+
     def __init__(self, num_games=3):
         """
         Initialize empty game_state.
-        
+
         :param num_games: int
             Number of games to play.
         """
@@ -17,8 +19,26 @@ class MTRPSEngine:
         self.num_games = num_games
         self.reset_game_state()
 
-
     def reset_game_state(self):
-        self.game_state = np.zeros(self.num_games, dtype=bool)
+        """Reset game state to all zeros."""
+        self.game_state = np.zeros(self.num_games)
 
+    def run(self, player1, player2):
+        """Run a set of games."""
+        self.reset_game_state()
+        for index in range(self.num_games):
+            p1_move = player1.make_move()
+            p2_move = player2.make_move()
 
+            if p1_move == p2_move:
+                # Draw
+                self.game_state[index] = 0
+            elif (p1_move - p2_move) == 1 or (p1_move - p2_move) == -2:
+                # player1 wins (Paper vs Rock or
+                #   Scissors vs Paper or Rock vs Scissors)
+                self.game_state[index] = 1
+            else:
+                # player2 wins
+                self.game_state[index] = 2
+
+        print(self.game_state)
