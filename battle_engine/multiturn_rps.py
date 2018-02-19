b12a165e2d66b95e38c1d332d2ba8a8172e3051e
Ali Turfah <aturfah@umich.edu>
2018-02-19 14:56:59 -0500

    Add header and parameter validation

--- battle_engine/multiturn_rps.py
+++ battle_engine/multiturn_rps.py
@@ -5,9 +5,16 @@ import numpy as np
 class MTRPSEngine:
     """Multiturn RPS Engine class."""
     def __init__(self, num_games=3):
-        """Initialize empty game_state"""
-        self.num_games = num_games
+        """
+        Initialize empty game_state.
+        
+        :param num_games: int
+            Number of games to play.
+        """
+        if num_games % 2 == 0:
+            raise AttributeError("num_games must be odd.")
 
+        self.num_games = num_games
         self.reset_game_state()
 
 
