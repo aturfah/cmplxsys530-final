1f760c5cd3f007ef7e0439bbc3cd54902bf106cf
Ali Turfah <aturfah@umich.edu>
2018-02-23 16:08:26 -0500

    ADd function to plot heatmaps of matchup matrices

--- stats/plot.py
+++ stats/plot.py
@@ -4,13 +4,15 @@ from cycler import cycler
 import matplotlib.pyplot as plt
 from matplotlib.widgets import CheckButtons
 
-COLORS = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
+import numpy as np
+
+COLORS = ["b", "g", "r", "c", "m", "y", "k", "w"]
 
 
 def plot_log_reader_data(log_reader):
     """Plot data from log_reader."""
     plt.subplots_adjust(right=0.8)
-    plt.rc('axes', prop_cycle=(
+    plt.rc("axes", prop_cycle=(
         cycler("color", COLORS[:len(log_reader.header)])))
 
     legend_info = log_reader.header
@@ -25,7 +27,7 @@ def plot_log_reader_data(log_reader):
         line_i, = plt.plot(log_reader.data[group], label=data_label)
         graph_dict[group] = line_i
 
-    plt.legend(legend_info, loc='upper left')
+    plt.legend(legend_info, loc="upper left")
     plt.ylabel("Average Elo Ranking")
 
     def new_data(event):
@@ -42,4 +44,38 @@ def plot_log_reader_data(log_reader):
         change_axis, legend_info, [True]*len(legend_info))
 
     change_buttons.on_clicked(new_data)
+    plt.show()
+
+def plot_matchup_matrix(colnames, matchup_matrix):
+    """Plot matchup matrix as heatmap."""
+    plot1_data = matchup_matrix[0,:,:]
+    plot2_data = matchup_matrix[1,:,:]
+
+    
+    ax = plt.subplot(121)
+    plt.title("W/L Ratio")
+    plt.imshow(plot1_data, cmap="coolwarm")
+    plt.grid(which="minor", lw=1, color="black")
+
+    ax.set_yticks(np.arange(plot1_data.shape[0]))
+    ax.set_xticks(np.arange(plot1_data.shape[1]))
+    ax.set_yticks([x - 0.5 for x in np.arange(1, plot1_data.shape[1])], minor=True)
+    ax.set_xticks([x - 0.5 for x in np.arange(1, plot1_data.shape[1])], minor=True)
+    ax.tick_params(axis="y",which="minor",bottom="off")
+    ax.tick_params(axis="x",which="minor",bottom="off")
+    ax.invert_yaxis()
+
+    ax.set_xticklabels(colnames, minor=False)
+    ax.set_yticklabels(colnames, minor=False)
+    plt.xticks(rotation=45)
+    
+    plt.colorbar()
+
+
+    plt.subplot(122)
+    plt.title("# of Games Played")
+    plt.imshow(plot2_data, cmap="coolwarm")
+    
+    plt.colorbar()
+
     plt.show()
