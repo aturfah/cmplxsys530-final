37b96d20d8505128339b351d266146573c2b16f7
Ali Turfah <aturfah@umich.edu>
2018-02-25 14:05:29 -0500

    Add docstring for individual turn calculations

--- battle_engine/pokemon.py
+++ battle_engine/pokemon.py
@@ -4,6 +4,17 @@ class PokemonEngine():
     """Class to run a pokemon game."""
     pass
 
-def calculate_turn():
-    """Calculate results of a turn of a pokemon game."""
+def calculate_turn(move1, player1, move2, player2):
+    """
+    Calculate results of a turn of a pokemon game.
+    
+    :param move1/move2: tuple
+        Player 1/2's move respectively. Tuple of size 2. First
+        value is the type of move (either SWITCH or ATTACK),
+        followed by the index of the attack or pokemon to be
+        switched to.
+    :param player1/player2: PokemonAgent
+        Instance that is Player 1/2 respectively. See
+        agent/pokemon_agent.py for documentation.
+    """
     pass
