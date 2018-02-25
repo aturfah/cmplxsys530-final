240790c906e984d024cc44de5f3acb8b06f540dd
Ali Turfah <aturfah@umich.edu>
2018-02-25 11:00:06 -0500

    Add preliminary pokemon move data (and load with config)

--- config.py
+++ config.py
@@ -1,7 +1,18 @@
 """Config file with directory info."""
 import os
+import json
 
 # Log file directory
 LOG_DIR = "logs/"
 if not os.path.isdir(LOG_DIR):
     os.mkdir(LOG_DIR)
+
+# Store move data
+MOVE_DATA = None
+with open("data/moves/sample_moves.json") as move_file:
+    MOVE_DATA = json.load(move_file)
+
+# Store pokemon (stats) data
+POKEMON_DATA = None
+with open("data/moves/sample_dex.json") as dex_file:
+    POKEMON_DATA = json.load(dex_file)
