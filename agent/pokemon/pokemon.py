28a9a3a800f72e63993c1d19fee754868d5961f3
Ali Turfah <aturfah@umich.edu>
2018-02-25 11:09:15 -0500

    Add preliminary file to make a Pokemon

---
+++ agent/pokemon/pokemon.py
@@ -0,0 +1,11 @@
+"""Class for a pokemon used by a PokemonAgent."""
+
+from config import MOVE_DATA
+from config import POKEMON_DATA
+
+class Pokemon:
+    """The pokemon class."""
+
+    def __init__(self, name, moves, level=100):
+        """Initialize a pokemon."""
+        pass
