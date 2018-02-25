458e54d28bc595ff47efbc4a028331d9abbd7d73
Ali Turfah <aturfah@umich.edu>
2018-02-25 11:31:00 -0500

    Add init method for Pokemon

--- agent/pokemon/pokemon.py
+++ agent/pokemon/pokemon.py
@@ -3,9 +3,24 @@
 from config import MOVE_DATA
 from config import POKEMON_DATA
 
+
 class Pokemon:
     """The pokemon class."""
 
     def __init__(self, name, moves, level=100):
         """Initialize a pokemon."""
-        pass
+        # Validate pokemon chosen
+        if name not in POKEMON_DATA:
+            raise AttributeError("Invalid pokemon chosen: {}.".format(name))
+
+        # Validate moves
+        if not moves:
+            raise AttributeError("Moves must be provided.")
+        for move in moves:
+            if move not in MOVE_DATA:
+                raise AttributeError("Invalid move chosen: {}.".format(move))
+
+        # Validate level
+        if level not in range(1, 101):
+            raise AttributeError("Level must be between 1 and 100")
+
