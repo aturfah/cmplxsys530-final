f62f4afc0f5b9f8329b273e0bcd1097d924a1221
Ali Turfah <aturfah@umich.edu>
2018-02-25 13:59:50 -0500

    Add pokemon engine

---
+++ battle_engine/pokemon.py
@@ -0,0 +1,9 @@
+"""Engine to run the turn of a pokemon game."""
+
+class PokemonEngine():
+    """Class to run a pokemon game."""
+    pass
+
+def calculate_turn():
+    """Calculate results of a turn of a pokemon game."""
+    pass
