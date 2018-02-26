9816410355dcd3fbb9069fc4eba9ca5f7427f583
Ali Turfah <aturfah@umich.edu>
2018-02-26 17:21:33 -0500

    Add test for when pokemon faint

--- tests/engine_tests/pokemon_engine_test.py
+++ tests/engine_tests/pokemon_engine_test.py
@@ -22,5 +22,21 @@ def test_run():
     assert p_eng.game_state["player2"]["active"] is None
     assert not p_eng.game_state["player2"]["team"]
 
+def test_run_multiple_pokemon():
+    """Test running a game with multiple pokemon"""
+    exploud = Pokemon("exploud", ["tackle"])
+    floatzel = Pokemon("floatzel", ["watergun"])
+
+    player1 = PokemonAgent([exploud, exploud, exploud, exploud])
+    player2 = PokemonAgent([floatzel, floatzel])
+
+    p_eng = PokemonEngine()
+
+    p_eng.run(player1, player2)
+    assert p_eng.game_state["player1"]["active"] is not None
+    assert p_eng.game_state["player1"]["team"]
+    assert p_eng.game_state["player2"]["active"] is None
+    assert not p_eng.game_state["player2"]["team"]
 
 test_run()
+test_run_multiple_pokemon()
