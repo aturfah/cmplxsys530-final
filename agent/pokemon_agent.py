ad53df1ff769af60c325a6692239d9ad11854cde
Ali Turfah <aturfah@umich.edu>
2018-02-25 19:06:55 -0500

    Add initial PokemonAgent method

--- agent/pokemon_agent.py
+++ agent/pokemon_agent.py
@@ -5,4 +5,9 @@ from agent.base_agent import BaseAgent
 class PokemonAgent(BaseAgent):
     """Class for a pokemon player."""
     
-    pass
+    def __init__(self, team):
+        """Initialize the agent."""
+        super().__init__(type="PokemonAgent")
+        self.team = team
+        self.gamestate = None
+    
