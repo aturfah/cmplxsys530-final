5da13c27017f622c1718104be177186a16c691af
Ali Turfah <aturfah@umich.edu>
2018-02-25 19:15:01 -0500

    Add skeleton code for running a game of pokemon

--- battle_engine/pokemon.py
+++ battle_engine/pokemon.py
@@ -2,19 +2,27 @@
 
 class PokemonEngine():
     """Class to run a pokemon game."""
-    pass
+    def __init__(self, generation, turn_limit=2000):
+        """Initialization method."""
+        self.generation = generation
+        self.turn_limit = turn_limit
+        self.reset_game_state()
 
-def calculate_turn(move1, player1, move2, player2):
-    """
-    Calculate results of a turn of a pokemon game.
+    def reset_game_state(self):
+        self.game_state = {}
     
-    :param move1/move2: tuple
-        Player 1/2's move respectively. Tuple of size 2. First
-        value is the type of move (either SWITCH or ATTACK),
-        followed by the index of the attack or pokemon to be
-        switched to.
-    :param player1/player2: PokemonAgent
-        Instance that is Player 1/2 respectively. See
-        agent/pokemon_agent.py for documentation.
-    """
-    pass
+    def run(self, player1, player2):
+        """Run a game of pokemon."""
+        pass
+        
+    def calculate_turn(self, move1, move2):
+        """
+        Calculate results of a turn of a pokemon game.
+
+        :param move1/move2: tuple
+            Player 1/2's move respectively. Tuple of size 2. First
+            value is the type of move (either SWITCH or ATTACK),
+            followed by the index of the attack or pokemon to be
+            switched to.
+        """
+        pass
