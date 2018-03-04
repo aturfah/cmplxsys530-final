5eebcf57ac91badaa0c8e2dd8a634f869227f715
Ali Turfah <aturfah@umich.edu>
2018-03-04 15:32:58 -0500

    Add parameter info in docstring

--- agent/pokemon_agent.py
+++ agent/pokemon_agent.py
@@ -27,12 +27,32 @@ class PokemonAgent(BaseAgent):
         self.opp_gamestate["moves"] = {}
 
     def update_gamestate(self, my_gamestate, opp_gamestate):
-        """Update internal gamestate for self."""
+        """
+        Update internal gamestate for self.
+        
+        :param my_gamestate: dict
+            PokemonEngine representation of player's position.
+            Should have "active" and "team" keys.
+        :param opp_gamestate: dict
+            PokemonEngine representation of opponent's position.
+            Only % HP should be viewable, and has "active" and
+            "team" keys.
+        """
         self.gamestate = my_gamestate
         self.opp_gamestate["data"] = opp_gamestate
 
     def new_info(self, turn_info, my_id):
-        """Get new info for opponent's game_state."""
+        """
+        Get new info for opponent's game_state.
+        
+        :param turn_info: list
+            What happened on that turn, who took what damage.
+            Each element should be a dict.
+        :param my_id: str
+            Name corresponding to the "attacker" or "defender"
+            values of this dict. To know which values the method
+            should be looking at.
+        """
         for info in turn_info:
             if info["attacker"] == my_id:
                 # We're the attacker
