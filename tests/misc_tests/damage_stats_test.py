c884df67ad775e7054bd353607b64360537fc98c
Ali Turfah <aturfah@umich.edu>
2018-03-09 08:59:40 -0500

    Test to find nearest number.

--- tests/misc_tests/damage_stats_test.py
+++ tests/misc_tests/damage_stats_test.py
@@ -7,4 +7,13 @@ def test_init():
     dsc = DamageStatCalc()
     assert dsc.damage_stats
 
+def test_nearest_num():
+    """Make sure it calculates the nearest number properly."""
+    dsc = DamageStatCalc()
+
+    assert dsc.find_closest_level(4) == (5, -1)
+    assert dsc.find_closest_level(215) == (200, 15)
+    assert dsc.find_closest_level(17) == (15, 2)
+
 test_init()
+test_nearest_num()
