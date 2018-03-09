5a190cc3e84875e915bf9daab9e15db1329f335c
Ali Turfah <aturfah@umich.edu>
2018-03-08 19:07:25 -0500

    Add initialization test

--- tests/misc_tests/damage_stats_test.py
+++ tests/misc_tests/damage_stats_test.py
@@ -1,8 +1,10 @@
 """Unit tests for damage_stats calculator."""
 
+from pokemon.damage_stats import DamageStatCalc
 
 def test_init():
     """Make sure everything initializes properly."""
-    pass
+    dsc = DamageStatCalc()
+    assert dsc.damage_stats
 
 test_init()
