0da15f9423fdca83cb5cc394c1a98f3c4383190b
Ali Turfah <aturfah@umich.edu>
2018-03-09 09:11:10 -0500

    Add test for estimating damage values

--- tests/misc_tests/damage_stats_test.py
+++ tests/misc_tests/damage_stats_test.py
@@ -2,10 +2,12 @@
 
 from pokemon.damage_stats import DamageStatCalc
 
+
 def test_init():
     """Make sure everything initializes properly."""
     dsc = DamageStatCalc()
     assert dsc.damage_stats
+
 
 def test_nearest_num():
     """Make sure it calculates the nearest number properly."""
@@ -15,5 +17,16 @@ def test_nearest_num():
     assert dsc.find_closest_level(215) == (200, 15)
     assert dsc.find_closest_level(17) == (15, 2)
 
+
+def test_estimate_dmg_val():
+    """Test calculation of stats."""
+    dsc = DamageStatCalc()
+
+    assert dsc.estimate_dmg_val(77) == 9.05
+    assert dsc.estimate_dmg_val(144, is_hp=True) == 20.43
+    assert dsc.estimate_dmg_val(88, is_atk=True) == 40.4
+
+
 test_init()
 test_nearest_num()
+test_estimate_dmg_val()
