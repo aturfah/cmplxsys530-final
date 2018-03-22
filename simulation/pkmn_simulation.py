97220a56ac3cc70b42376c82b026b2defdf895b2
Ali Turfah <aturfah@umich.edu>
2018-03-22 11:40:13 -0400

    Add dummy add_agents() method

--- simulation/pkmn_simulation.py
+++ simulation/pkmn_simulation.py
@@ -3,7 +3,7 @@
 from simulation.base_simulation import BaseSimulation
 from battle_engine.pokemon_engine import PokemonEngine
 from agent.pokemon_agent import PokemonAgent
-
+from pokemon_helpers.pokemon import Pokemon
 
 class PokemonSimulation(BaseSimulation):
     """Class for Pokemon Simulation."""
@@ -19,9 +19,31 @@ class PokemonSimulation(BaseSimulation):
 
     def add_agents(self):
         """Add the agents to this model."""
-        for _ in range(self.num_players):
-            print("ADDING AGENT #{}".format(_))
+        for ind in range(self.num_players):
+            if ind % 3 == 0:
+                pkmn_agent = PokemonAgent(default_team_exploud())
+                pkmn_agent.type = "exploud"
+            elif ind % 3 == 1:
+                pkmn_agent = PokemonAgent(default_team_floatzel())
+                pkmn_agent.type = "floatzel"
+            else:
+                pkmn_agent = PokemonAgent(default_team_spinda())
+                pkmn_agent.type = "spinda"
+            self.ladder.add_player(pkmn_agent)
+            print("ADDING AGENT {}".format(pkmn_agent.type))
 
     def run(self):
         """Run this simulation."""
         print("RUNNING!!!")
+
+def default_team_exploud():
+    """Generate an Exploud for these players."""
+    return Pokemon(name="exploud", moves=["tackle"])
+
+def default_team_spinda():
+    """Generate a Spinda for these players."""
+    return Pokemon(name="spinda", moves=["tackle"])
+
+def default_team_floatzel():
+    """Generate a FLoatzel for the player."""
+    return Pokemon(name="floatzel", moves=["watergun"])
