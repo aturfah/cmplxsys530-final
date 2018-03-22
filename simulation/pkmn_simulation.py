2eb0ee9c31edaeb1c26f4738ad8c9e4aa791b216
Ali Turfah <aturfah@umich.edu>
2018-03-22 12:00:32 -0400

    Add player log writer

--- simulation/pkmn_simulation.py
+++ simulation/pkmn_simulation.py
@@ -4,6 +4,8 @@ from simulation.base_simulation import BaseSimulation
 from battle_engine.pokemon_engine import PokemonEngine
 from agent.pokemon_agent import PokemonAgent
 from pokemon_helpers.pokemon import Pokemon
+from log_manager.log_writer import LogWriter
+from stats.calc import calculate_avg_elo
 
 class PokemonSimulation(BaseSimulation):
     """Class for Pokemon Simulation."""
@@ -13,6 +15,8 @@ class PokemonSimulation(BaseSimulation):
         pkmn_kwargs = kwargs
         pkmn_kwargs["game"] = PokemonEngine()
         pkmn_kwargs["prefix"] = "PKMN"
+        self.type_log_writer = None
+        self.data_delay = kwargs["data_delay"]
         super().__init__(pkmn_kwargs)
 
         print("POKEMON SIMULATION!!!")
@@ -34,9 +38,24 @@ class PokemonSimulation(BaseSimulation):
 
     def run(self):
         """Run this simulation."""
-        for _ in range(self.num_games):
+        for game_ind in range(self.num_games):
             outcome, player1, player2 = self.ladder.run_game()
             self.write_player_log(outcome, player1, player2)
+
+            if game_ind % self.data_delay == 0:
+                # Calculate the average ranking statistics
+                # every <data_delay> iterations
+                self.type_log_writer.write_line(calculate_avg_elo(self.ladder))
+
+    def init_type_log_writer(self):
+        """Initialize Type Average Elo LogWriter."""
+        header = []
+        header.append("exploud")
+        header.append("spinda")
+        header.append("floatzel")
+
+        self.type_log_writer = LogWriter(header, prefix="RPSTypes")
+
 
 def default_team_exploud():
     """Generate an Exploud for these players."""
