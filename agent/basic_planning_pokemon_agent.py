d2e14d2d8a4494add6dd4bbe736fcc1b60613c99
Ali Turfah <aturfah@umich.edu>
2018-03-27 11:52:20 -0400

    Add preliminary code for the basic_planning pokemon agent

---
+++ agent/basic_planning_pokemon_agent.py
@@ -0,0 +1,13 @@
+"""Pokemon agent who's moves are determined by maximizing personal gain."""
+
+from agent.basic_pokemon_agent import PokemonAgent
+
+
+class BasicPlanningPokemonAgent(PokemonAgent):
+    """
+    Class for PokemonAgent who calculates the next move by maximizing some function.
+
+    This agent will maximize the game_position given the opponent's moves are all
+    equally likely.
+    """
+    pass
