4bb3051d30c9b86733bfaff070723dadbdddda69
Ali Turfah <aturfah@umich.edu>
2018-03-27 12:34:51 -0400

    Add code to generate moves under uncertainty

--- agent/basic_planning_pokemon_agent.py
+++ agent/basic_planning_pokemon_agent.py
@@ -3,6 +3,8 @@
 from agent.basic_pokemon_agent import PokemonAgent
 
 from config import USAGE_STATS
+import operator
+
 
 class BasicPlanningPokemonAgent(PokemonAgent):
     """
@@ -11,6 +13,7 @@ class BasicPlanningPokemonAgent(PokemonAgent):
     This agent will maximize the game_position given the opponent's moves are all
     equally likely.
     """
+
     def __init__(self, tier, **kwargs):
         """Initialize a player with a specific tier."""
         super().__init__(*kwargs)
@@ -19,6 +22,8 @@ class BasicPlanningPokemonAgent(PokemonAgent):
     def make_move(self):
         """Choose the move to make."""
         player_opts, opp_opts = self.generate_possibilities()
+        print(player_opts)
+        print(opp_opts)
 
     def generate_possibilities(self):
         """Generate a two lists of possible player and opponent moves."""
@@ -45,12 +50,22 @@ class BasicPlanningPokemonAgent(PokemonAgent):
             for move in self.opp_gamestate["moves"][opp_active_poke]:
                 opp_moves.append(move)
         if len(opp_moves) < 4:
-            print(USAGE_STATS[self.tier][opp_active_poke])
+            common_moves = USAGE_STATS[self.tier][opp_active_poke]["Moves"]
+            common_moves = sorted(common_moves.items(),
+                                  key=operator.itemgetter(1), reverse=True)
+            common_moves = [move[0] for move in common_moves if move[0] != ""]
+            for move in common_moves:
+                if move not in opp_moves:
+                    opp_moves.append(move)
+                if len(opp_moves) == 4:
+                    break
+        for move in opp_moves:
+            opp_opts.append(("ATTACK", move))
 
         # Opponent's possible switches
         posn = 0
         for poke in self.opp_gamestate["data"]["team"]:
             poke_name = poke["name"]
-
+            opp_opts.append(("SWITCH", poke_name))
 
         return player_opts, opp_opts
