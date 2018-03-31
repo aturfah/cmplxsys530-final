f3c522e42b19c997c96f1301b134e4221c99361a
Ali Turfah <aturfah@umich.edu>
2018-03-31 10:38:37 -0400

    Choose the moves name

--- agent/basic_planning_pokemon_agent.py
+++ agent/basic_planning_pokemon_agent.py
@@ -18,7 +18,8 @@ class BasicPlanningPokemonAgent(PokemonAgent):
 
     def __init__(self, tier, **kwargs):
         """Initialize a player with a specific tier."""
-        super().__init__(*kwargs)
+        team = kwargs["team"]
+        super().__init__(team)
         self.tier = tier
 
     def make_move(self):
@@ -50,7 +51,7 @@ class BasicPlanningPokemonAgent(PokemonAgent):
         opp_moves = []
         if opp_active_poke in self.opp_gamestate["moves"]:
             for move in self.opp_gamestate["moves"][opp_active_poke]:
-                opp_moves.append(move)
+                opp_moves.append(move["name"])
         if len(opp_moves) < 4:
             common_moves = USAGE_STATS[self.tier][opp_active_poke]["Moves"]
             common_moves = sorted(common_moves.items(),
