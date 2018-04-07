9ff76dd9aa87fc97732b3af89c40c2649d079812
Ali Turfah <aturfah@umich.edu>
2018-04-07 13:25:54 -0400

    Add code to initialize the game for the two players

--- battle_engine/interactive_pokemon_engine.py
+++ battle_engine/interactive_pokemon_engine.py
@@ -1,5 +1,7 @@
 """Pokemon Engine for the player to play against."""
 
+from copy import deepcopy
+
 from battle_engine.pokemon_engine import PokemonEngine
 
 class InteractivePokemonEngine(PokemonEngine):
@@ -11,7 +13,26 @@ class InteractivePokemonEngine(PokemonEngine):
 
     def initialize_battle(self, player1, player2):
         """Initialize this battle and set the players' gamestates."""
-        pass
+        # Initialize the players' teams
+        self.game_state["player1"]["team"] = deepcopy(player1.team)
+        self.game_state["player2"]["team"] = deepcopy(player2.team)
+
+        # Each player leads with first pokemon on their side
+        self.game_state["player1"]["active"] = \
+            self.game_state["player1"]["team"].pop(0)
+        self.game_state["player2"]["active"] = \
+            self.game_state["player2"]["team"].pop(0)
+
+        # Set initial game states for players
+        player1.update_gamestate(
+            self.game_state["player1"], self.anonymize_gamestate("player2"))
+        player2.update_gamestate(
+            self.game_state["player2"], self.anonymize_gamestate("player1"))
+
+        player1.init_opp_gamestate(self.game_state["player2"]["team"],
+                                   self.game_state["player2"]["active"])
+        player2.init_opp_gamestate(self.game_state["player1"]["team"],
+                                   self.game_state["player1"]["active"])
 
     def run_turn(self, player_move, player2):
         """Run a turn of this battle."""
