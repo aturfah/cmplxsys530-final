aede54cb5abca264b643ee929a335c375e6c53e7
Ali Turfah <aturfah@umich.edu>
2018-04-06 22:03:51 -0400

    Add options for Rock/Paper/Scissors/Uniform agents

--- interface/static/func.js
+++ interface/static/func.js
@@ -1,4 +1,4 @@
-function submit_form(){
+function submit_form() {
     var game_choice = document.getElementById("game_dropdown").value;
     var opp_choice = document.getElementById("opp_dropdown").value;
     var data = {
@@ -14,12 +14,12 @@ function submit_form(){
 
 function update_opp_choices(game_choice) {
     var opp_choices = document.getElementById("opp_dropdown");
-    
+
     // Clear the list
     opp_choices.options.length = 0;
-    
+
     // Repopulate
-    options = []
+    var options = [];
     if (game_choice == "rps") {
         var option1 = document.createElement("option");
         option1.text = "Random";
@@ -27,9 +27,25 @@ function update_opp_choices(game_choice) {
         var option2 = document.createElement("option");
         option2.text = "Counter";
         option2.value = "counter_rps"
+        var option3 = document.createElement("option");
+        option3.text = "Rock";
+        option3.value = "rock_rps";
+        var option4 = document.createElement("option");
+        option4.text = "Paper";
+        option4.value = "paper_rps";
+        var option5 = document.createElement("option");
+        option5.text = "Scissors";
+        option5.value = "scissors_rps";
+        var option6 = document.createElement("option");
+        option6.text = "Uniform";
+        option6.value = "uniform_rps";
 
-        options.push(option1)
-        options.push(option2)
+        options.push(option1);
+        options.push(option2);
+        options.push(option3);
+        options.push(option4);
+        options.push(option5);
+        options.push(option6);
     } else {
         var option1 = document.createElement("option");
         option1.text = "Random";
@@ -42,13 +58,13 @@ function update_opp_choices(game_choice) {
         options.push(option2)
     }
 
-    options.forEach(function(option){
+    options.forEach(function (option) {
         opp_choices.add(option)
     });
     update_opp_team(game_choice)
 }
 
-function update_opp_team(game_choice){
+function update_opp_team(game_choice) {
     var team_dropdown = document.getElementById("opp_team");
 
     // Clear the list
@@ -56,7 +72,7 @@ function update_opp_team(game_choice){
 
     // REpopulate
     var options = []
-    if (game_choice === "rps"){
+    if (game_choice === "rps") {
         var option1 = document.createElement("option");
         option1.text = "N/A";
         options.push(option1)
@@ -75,7 +91,7 @@ function update_opp_team(game_choice){
         options.push(option3)
     }
 
-    options.forEach(function(option){
+    options.forEach(function (option) {
         team_dropdown.add(option);
     });
 }
