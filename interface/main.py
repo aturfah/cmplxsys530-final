8f341709d00b023d95a575131871b9a067e48516
Ali Turfah <aturfah@umich.edu>
2018-04-06 21:05:12 -0400

    Add index endpoint

--- interface/main.py
+++ interface/main.py
@@ -1,1 +1,11 @@
 """Main Flask App for Agent Interface"""
+
+from flask import Flask
+from flask import render_template
+
+INTERFACE = Flask(__name__)
+
+@INTERFACE.route("/")
+def index():
+    """Index page."""
+    return render_template('index.html')
