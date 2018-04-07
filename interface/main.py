e1e891531dfbb305912b4b5127e81c616bae880c
Ali Turfah <aturfah@umich.edu>
2018-04-06 21:29:10 -0400

    Update the internal ENIGNE attribute

--- interface/main.py
+++ interface/main.py
@@ -18,16 +18,23 @@ ENGINE_DICT = {
     "pkmn": PokemonEngine
 }
 
+OPPONENT = None
+
 @INTERFACE.route("/")
 def index():
     """Index page."""
+    global ENGINE
+    engine = None
     return render_template('index.html')
 
 @INTERFACE.route("/set_parameters", methods=["POST"])
 def set_engine():
     """Set the game for this interface."""
+    global ENGINE
+
     req_data = json.loads(request.data)
 
     game_choice = req_data["game_choice"]
+    ENGINE = ENGINE_DICT[game_choice]()
 
     return jsonify({})
