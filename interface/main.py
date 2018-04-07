c8549994381e1727a1920d1a78df8f3b19915c4c
Ali Turfah <aturfah@umich.edu>
2018-04-06 21:10:57 -0400

    Add the preliminary code for the settiing of the game

--- interface/main.py
+++ interface/main.py
@@ -1,11 +1,30 @@
 """Main Flask App for Agent Interface"""
 
 from flask import Flask
+from flask import jsonify
 from flask import render_template
+from flask import request
+
+from battle_engine.rockpaperscissors import RPSEngine
+from battle_engine.pokemon_engine import PokemonEngine
 
 INTERFACE = Flask(__name__)
+
+ENGINE = None
+ENGINE_DICT = {
+    "rps": RPSEngine,
+    "pkmn": PokemonEngine
+}
 
 @INTERFACE.route("/")
 def index():
     """Index page."""
     return render_template('index.html')
+
+@INTERFACE.route("/set_engine", methods=["POST"])
+def set_engine():
+    """Set the game for this interface."""
+    choice = request.form.to_dict(flat=False)
+    print(choice)
+
+    return jsonify({})
