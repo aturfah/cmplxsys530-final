9c2b1edf4926b0ae4019bad0bb0ae4a19d317695
Ali Turfah <aturfah@umich.edu>
2018-04-08 13:39:14 -0400

    Add the pokemon's images to the page

--- interface/static/func.js
+++ interface/static/func.js
@@ -115,4 +115,50 @@ function update_team(game_choice) {
 
 function set_opts(options) {
     console.log(options)
+    // Set player's active Pokemon
+    var game_window = document.getElementById("game_window")
+    typeof(create_poke_DOM(options["player_active"], false))
+    game_window.appendChild(create_poke_DOM(options["player_active"], false))
+    game_window.appendChild(create_poke_DOM(options["opp_active"], true))
+}
+
+function create_poke_DOM(data, opponent){
+    // Create the pokemon display from the data given.
+    var url = "https://play.pokemonshowdown.com/sprites/afd/REPLACE.png".replace("REPLACE", data["name"]);
+    var hp_pct = "REPLACE%".replace("REPLACE", data["current_hp"]*100/data["max_hp"]);
+    var hp_raw = "(REPLACE1/REPLACE2)".replace("REPLACE1", data["current_hp"])
+                                    .replace("REPLACE2", data["max_hp"]);
+    
+    var poke_div = document.createElement("div")
+    poke_div.style.width = "40%"
+    poke_div.style.display = "inline-block"
+    // Set player/Opponent box
+    var title = document.createElement("h5")
+    if (opponent) {
+        title.innerHTML = "".concat("Opponent's ", data["name"])
+    } else {
+        title.innerHTML = "".concat("Player's ", data["name"])
+    }
+    // Set image
+    var poke_img = document.createElement("img")
+    poke_img.src = url
+    // Set HP String
+    poke_hp_text = "Current HP: "
+    poke_hp_text = poke_hp_text.concat(hp_pct)
+    if (!opponent) {
+        poke_hp_text = poke_hp_text.concat(" ", hp_raw)
+    }
+    
+    var poke_hp = document.createElement("p")
+    poke_hp.innerHTML = poke_hp_text
+
+    poke_div.appendChild(title)
+    poke_div.appendChild(poke_img)
+    poke_div.appendChild(poke_hp)
+
+    return poke_div
+}
+
+function create_move_DOM(moves) {
+    console.log(moves)
 }
