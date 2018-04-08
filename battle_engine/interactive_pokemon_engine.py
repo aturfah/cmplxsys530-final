c789f3b5833e2fa907d496778a9b472bccf8fcff
Ali Turfah <aturfah@umich.edu>
2018-04-08 14:08:54 -0400

    Write logic for the processing of the turn

--- battle_engine/interactive_pokemon_engine.py
+++ battle_engine/interactive_pokemon_engine.py
@@ -34,6 +34,39 @@ class InteractivePokemonEngine(PokemonEngine):
         player2.init_opp_gamestate(self.game_state["player1"]["team"],
                                    self.game_state["player1"]["active"])
 
-    def run_turn(self, player_move, player2):
+    def run_turn(self, player1_move, player1, player2):
         """Run a turn of this battle."""
-        pass
+        player2_move = player2.make_move()
+        turn_info = self.calculate_turn(player1_move, player2_move)
+
+        player1.new_info(turn_info, "player1")
+        player2.new_info(turn_info, "player2")
+
+        # Figure out who faints at the end of this turn.
+        if self.game_state["player1"]["active"].current_hp < 0:
+            self.game_state["player1"]["active"] = None
+        if self.game_state["player2"]["active"].current_hp < 0:
+            self.game_state["player2"]["active"] = None
+
+        self.update_gamestates(player1, player2)
+
+        # If battle is not over, switch in next pokemon.
+        outcome = self.win_condition_met()
+        if not outcome["finished"]:
+            update = False
+            if self.game_state["player1"]["active"] is None:
+                switchin_ind = player1.switch_faint()
+                self.game_state["player1"]["active"] = \
+                    self.game_state["player1"]["team"].pop(switchin_ind)
+                update = True
+
+            if self.game_state["player2"]["active"] is None:
+                switchin_ind = player2.switch_faint()
+                self.game_state["player2"]["active"] = \
+                    self.game_state["player2"]["team"].pop(switchin_ind)
+                update = True
+
+            if update:
+                self.update_gamestates(player1, player2)
+
+        return turn_info, outcome
