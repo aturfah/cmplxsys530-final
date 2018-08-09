b9de780f986bf22d53c69e560390fdb174d248a5
Ali Turfah <aturfah@umich.edu>
2018-08-09 17:03:49 -0400

    Add preliminary TeamReader class

---
+++ log_manager/team_reader.py
@@ -0,0 +1,12 @@
+"""Class to read teams from a directory."""
+
+class TeamReader:
+    """
+    Team Reader Class.
+
+    Attributes:
+        None.
+    """
+    def __init__(self):
+        """Initialization method."""
+        pass
