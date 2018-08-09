df15d406b5ca0c80e691e845efd8080858f9d058
Ali Turfah <aturfah@umich.edu>
2018-08-09 17:21:19 -0400

    Shuffle some lines of code around to make more sense

--- tests/misc_tests/team_reader_test.py
+++ tests/misc_tests/team_reader_test.py
@@ -4,8 +4,13 @@ from log_manager.team_reader import TeamReader
 
 def test_init():
     """Test initialization method for TeamReader."""
-    tr1 = TeamReader()
-    tr2 = TeamReader(prefix="p")
-    tr3 = TeamReader(suffix="q")
+    # Check that reading files works properly
+    tr_reg = TeamReader()
+    tr_pre = TeamReader(prefix="p")
+    tr_suf = TeamReader(suffix="q")
+
+    assert tr_reg.team_files
+    assert not tr_pre.team_files
+    assert not tr_suf.team_files
 
 test_init()
