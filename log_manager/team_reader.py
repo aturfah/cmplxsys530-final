6fee78aa2a0f8f7be70047adc8181930aff4a02a
Ali Turfah <aturfah@umich.edu>
2018-08-09 17:15:55 -0400

    Prefix/Suffix support

--- log_manager/team_reader.py
+++ log_manager/team_reader.py
@@ -2,14 +2,26 @@
 
 from os import listdir
 
+
 class TeamReader:
     """
     Team Reader Class.
 
     Attributes:
-        None.
+        team_files = List of filenames for the teams
     """
-    def __init__(self, teams_directory="data/teams"):
+
+    def __init__(self, teams_directory="data/teams", prefix=None, suffix=None):
         """Initialization method."""
-        teams_list = listdir(teams_directory)
-        print(teams_list)
+        team_file_list = listdir(teams_directory)
+
+        if prefix:
+            team_file_list = [
+                team_file for team_file in team_file_list if team_file.startswith(prefix)]
+
+        if suffix:
+            team_file_list = [
+                team_file for team_file in team_file_list if team_file.endswith(suffix)]
+
+        print(team_file_list)
+        self.team_files = team_file_list
