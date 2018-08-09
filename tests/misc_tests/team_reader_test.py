b86e7d818f0e8db1d5152d0b924133b87d29ebb7
Ali Turfah <aturfah@umich.edu>
2018-08-09 17:09:55 -0400

    Update init test

--- tests/misc_tests/team_reader_test.py
+++ tests/misc_tests/team_reader_test.py
@@ -1,7 +1,9 @@
 """Testing file for TeamReader."""
 
+from log_manager.team_reader import TeamReader
+
 def test_init():
     """Test initialization method for TeamReader."""
-    pass
+    tr1 = TeamReader()
 
 test_init()
