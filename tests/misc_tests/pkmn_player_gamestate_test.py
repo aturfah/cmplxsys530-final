363ebc1739093edef442ac2d5f0d5c0fbb957da7
Ali Turfah <aturfah@umich.edu>
2018-10-10 15:41:51 -0400

    Preliminary tests

---
+++ tests/misc_tests/pkmn_player_gamestate_test.py
@@ -0,0 +1,11 @@
+"""Testing script for the pkmn_player_gamestate class."""
+
+from pokemon_helpers.pkmn_player_gamestate import PokemonPlayerGameState
+
+def basic_test():
+    """Test initializing and accessing of attributes."""
+    ppgs = PokemonPlayerGameState()
+    assert ppgs.test == "DOOT"
+    assert ppgs["test"] == "pew"
+
+basic_test()
