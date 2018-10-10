037580fa2b91414cc496f61a62a665cbd6870c6d
Ali Turfah <aturfah@umich.edu>
2018-10-10 18:27:46 -0400

    Add imports & methods to reset gamestate

--- pokemon_helpers/pkmn_player_gamestate.py
+++ pokemon_helpers/pkmn_player_gamestate.py
@@ -1,5 +1,8 @@
 """Class defining an Engine's Game State."""
 
+from pokemon_helpers.calculate import calculate_spe_range
+from pokemon_helpers.calculate import generate_all_ev_combinations
+from pokemon_helpers.damage_stats import DamageStatCalc
 
 class PokemonPlayerGameState:
     """Representation of a player's internal game state."""
@@ -7,6 +10,22 @@ class PokemonPlayerGameState:
     def __init__(self):
         """Initialize this player's internal game state."""
         self.test_attr = {}
+        self.gamestate = {}
+        self.opp_gamestate = {}
+        self.opp_gamestate["data"] = {}
+        self.opp_gamestate["moves"] = {}
+        self.opp_gamestate["investment"] = {}
+        self.dmg_stat_calc = DamageStatCalc()
+
+
+    def reset_gamestates(self):
+        """Reset gamestate values for a new battle."""
+        self.gamestate = {}
+        self.opp_gamestate = {}
+        self.opp_gamestate["data"] = {}
+        self.opp_gamestate["moves"] = {}
+        self.opp_gamestate["investment"] = {}
+
 
     def __getitem__(self, key):
         """
