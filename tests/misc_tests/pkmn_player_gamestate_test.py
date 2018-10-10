c61fd7e60dc9e559b2e70bb3889ccb495acac743
Ali Turfah <aturfah@umich.edu>
2018-10-10 16:17:59 -0400

    Include import

--- tests/misc_tests/pkmn_player_gamestate_test.py
+++ tests/misc_tests/pkmn_player_gamestate_test.py
@@ -1,5 +1,7 @@
 """Testing script for the pkmn_player_gamestate class."""
 
+from pokemon_helpers.pkmn_player_gamestate import PokemonPlayerGameState
+
 def basic_test():
     """Test initializing and accessing of attributes."""
     ppgs = PokemonPlayerGameState()
