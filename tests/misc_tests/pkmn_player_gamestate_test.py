17a305d89a1d0b19c76fbf727ccf4fe9962dee55
Ali Turfah <aturfah@umich.edu>
2018-10-10 16:23:07 -0400

    Add test for setting attribute

--- tests/misc_tests/pkmn_player_gamestate_test.py
+++ tests/misc_tests/pkmn_player_gamestate_test.py
@@ -6,7 +6,13 @@ from pokemon_helpers.pkmn_player_gamestate import PokemonPlayerGameState
 def basic_test():
     """Test initializing and accessing of attributes."""
     ppgs = PokemonPlayerGameState()
-    assert ppgs.test
+
+    # Attribute lookup
+    assert not ppgs.test_attr
+
+    # Setting values in that dictionary
+    ppgs.test_attr["test"] = 7
+    assert ppgs.test_attr["test"]
 
 
 basic_test()
