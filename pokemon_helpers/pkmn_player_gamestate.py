1e0ef772375f4cf5ca72b360583684082f4d5dd0
Ali Turfah <aturfah@umich.edu>
2018-10-10 16:17:17 -0400

    Remove set_attr (for now)

--- pokemon_helpers/pkmn_player_gamestate.py
+++ pokemon_helpers/pkmn_player_gamestate.py
@@ -1,7 +1,7 @@
 """Class defining an Engine's Game State."""
 
 
-class PokemonPlayerGameState():
+class PokemonPlayerGameState:
     """Representation of a player's internal game state."""
 
     def __init__(self):
@@ -16,15 +16,3 @@ class PokemonPlayerGameState():
             Attribute of this object to get.
         """
         return self.__getattribute__(key)
-
-    def __setattr__(self, key, value):
-        """
-        Define [] setting on this object.
-
-        :param key: str
-            Attribute of this object to get.
-        :param value: Object
-            Value to set for attribute
-
-        """
-        pass
