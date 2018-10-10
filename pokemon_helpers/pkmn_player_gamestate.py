84c605338be7c069e99e99dbcb8b007ca60a53f0
Ali Turfah <aturfah@umich.edu>
2018-10-10 18:30:45 -0400

    c/p init_opp_gamestate code

--- pokemon_helpers/pkmn_player_gamestate.py
+++ pokemon_helpers/pkmn_player_gamestate.py
@@ -25,6 +25,35 @@ class PokemonPlayerGameState:
         self.opp_gamestate["moves"] = {}
         self.opp_gamestate["investment"] = {}
 
+    def init_opp_gamestate(self, opp_team, opp_active):
+        """
+        Initialize the investment data for the opponent's team.
+
+        Args:
+            opp_team (list): List with the opponent's Pokemon.
+            opp_active (Pokemon): Opponent's active Pokemon.
+
+        """
+        possible_combs = generate_all_ev_combinations()
+        self.opp_gamestate["investment"][opp_active["name"]] = {}
+        self.opp_gamestate["investment"][opp_active["name"]]["hp"] = possible_combs["hp"]
+        self.opp_gamestate["investment"][opp_active["name"]]["atk"] = possible_combs["atk"]
+        self.opp_gamestate["investment"][opp_active["name"]]["def"] = possible_combs["def"]
+        self.opp_gamestate["investment"][opp_active["name"]]["spa"] = possible_combs["spa"]
+        self.opp_gamestate["investment"][opp_active["name"]]["spd"] = possible_combs["spd"]
+        self.opp_gamestate["investment"][opp_active["name"]]["spe"] = \
+            calculate_spe_range(opp_active["name"])
+
+        for opp_poke in opp_team:
+            self.opp_gamestate["investment"][opp_poke["name"]] = {}
+            self.opp_gamestate["investment"][opp_poke["name"]]["hp"] = possible_combs["hp"]
+            self.opp_gamestate["investment"][opp_poke["name"]]["atk"] = possible_combs["atk"]
+            self.opp_gamestate["investment"][opp_poke["name"]]["def"] = possible_combs["def"]
+            self.opp_gamestate["investment"][opp_poke["name"]]["spa"] = possible_combs["spa"]
+            self.opp_gamestate["investment"][opp_poke["name"]]["spd"] = possible_combs["spd"]
+            self.opp_gamestate["investment"][opp_poke["name"]]["spe"] = \
+                calculate_spe_range(opp_poke["name"])
+
     def __getitem__(self, key):
         """
         Define [] lookup on this object.
