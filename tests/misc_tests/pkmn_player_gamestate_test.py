b0b646d3585b2bb751ef4bd66ca48cf08de4eee5
Ali Turfah <aturfah@umich.edu>
2018-10-10 16:19:13 -0400

    Spacing

--- tests/misc_tests/pkmn_player_gamestate_test.py
+++ tests/misc_tests/pkmn_player_gamestate_test.py
@@ -2,10 +2,11 @@
 
 from pokemon_helpers.pkmn_player_gamestate import PokemonPlayerGameState
 
+
 def basic_test():
     """Test initializing and accessing of attributes."""
     ppgs = PokemonPlayerGameState()
     assert ppgs.test
-    print(ppgs["test"])
+
 
 basic_test()
