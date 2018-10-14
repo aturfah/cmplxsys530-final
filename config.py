4c727e992a20efc8073734c000d31605cf7714b0
Ali Turfah <aturfah@umich.edu>
2018-10-14 13:25:23 -0400

    Add constatns for status strings

--- config.py
+++ config.py
@@ -142,3 +142,9 @@ for poke_name in USAGE_STATS["pu"]:
     TEMP_DICT[mod_name] = USAGE_STATS["pu"][poke_name]
 USAGE_STATS["pu"] = TEMP_DICT
 del TEMP_DICT
+
+# Status strings
+PAR_STATUS = "par"
+FRZ_STATUS = "frz"
+SLP_STATUS = "slp"
+BRN_STATUS = "brn"
