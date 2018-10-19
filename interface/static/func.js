5e9b0e483240759a0d0ca69164ef684f6bc9dfaa
Ali Turfah <aturfah@umich.edu>
2018-10-19 19:06:18 -0400

    Show moves for all pokemon

--- interface/static/func.js
+++ interface/static/func.js
@@ -327,18 +327,16 @@ function create_player_pkmn_panel(pkmn_data, active) {
     }
 
     // Display Pokemon's moves, if not active
-    if (active === false) {
-        var moves_label = document.createElement("li");
-        moves_label.innerHTML = "<b>Moves:</b>";
-        var move_sublist = document.createElement("ul");
-        pkmn_data["moves"].forEach(function (move_info){
-            let move_li = document.createElement("li");
-            move_li.innerHTML = move_info["name"];
-            move_sublist.appendChild(move_li);
-        });
-        data_list.appendChild(moves_label);
-        data_list.appendChild(move_sublist);
-    }
+    var moves_label = document.createElement("li");
+    moves_label.innerHTML = "<b>Moves:</b>";
+    var move_sublist = document.createElement("ul");
+    pkmn_data["moves"].forEach(function (move_info){
+        let move_li = document.createElement("li");
+        move_li.innerHTML = move_info["name"];
+        move_sublist.appendChild(move_li);
+    });
+    data_list.appendChild(moves_label);
+    data_list.appendChild(move_sublist);
 
     // Display pokemon's stats, and boosts (if active)
     var stats = [["attack", "atk"], ["defense", "def"], ["sp_attack","spa"], ["sp_defense", "spd"], ["speed", "spe"]];
