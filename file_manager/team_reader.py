61548bed16b916aace82ec9b74ddf431bacdbe7f
Ali Turfah <aturfah@umich.edu>
2018-10-19 12:51:44 -0400

    Revert commit

--- file_manager/team_reader.py
+++ file_manager/team_reader.py
@@ -150,7 +150,6 @@ def process_pokemon(pokemon_dict):
     """Generate a Pokemon from the string in a file."""
     init_dict = {}
     init_dict["nickname"] = pokemon_dict["species"]
-    init_dict["nature"] = pokemon_dict.get("nature", "serious")
     init_dict["name"] = pokemon_dict["species"]
     init_dict["item"] = pokemon_dict["item"]
     init_dict["gender"] = pokemon_dict["gender"]
