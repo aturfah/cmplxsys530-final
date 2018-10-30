60917bde17d88a7358abdad2a330bdf5c11e03a2
Ali Turfah <aturfah@umich.edu>
2018-10-30 09:49:03 -0400

    Adjust tests

--- tests/agent_tests/adjusting_rps_agent_test.py
+++ tests/agent_tests/adjusting_rps_agent_test.py
@@ -5,10 +5,16 @@ from agent.adjusting_rps_agent import AdjustingRPSAgent
 
 def test_init():
     """Test Initialization method."""
+    # Test that initializes default
     arps = AdjustingRPSAgent()
-    assert arps.weight == 3
+    assert arps.weight == 1
     assert arps.original_strategy == arps.strategy
-    assert arps.counts == [1, 1, 1]
+    assert arps.counts == [1/3, 1/3, 1/3]
+
+    # Tests when weights are not defaults
+    arps_weight = AdjustingRPSAgent(weight=6)
+    assert arps_weight.weight == 6
+    assert arps_weight.counts == [2, 2, 2]
 
 
 test_init()
