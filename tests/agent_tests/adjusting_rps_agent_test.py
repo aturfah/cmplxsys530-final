60190cb22b61164246d1bfa8dbc2ba88951402d6
Ali Turfah <aturfah@umich.edu>
2018-10-30 09:52:25 -0400

    Move weights tests to their own function

--- tests/agent_tests/adjusting_rps_agent_test.py
+++ tests/agent_tests/adjusting_rps_agent_test.py
@@ -11,10 +11,20 @@ def test_init():
     assert arps.original_strategy == arps.strategy
     assert arps.counts == [1/3, 1/3, 1/3]
 
+    test_init_weights()
+
+
+def test_init_weights():
+    """Tests weights formatting of initialization."""
     # Tests when weights are not defaults
-    arps_weight = AdjustingRPSAgent(weight=6)
-    assert arps_weight.weight == 6
-    assert arps_weight.counts == [2, 2, 2]
+    arps_int = AdjustingRPSAgent(weight=6)
+    assert arps_int.weight == 6
+    assert arps_int.counts == [2, 2, 2]
+
+    # Test non-integer weights
+    arps_float = AdjustingRPSAgent(weight=2)
+    assert arps_float.weight == 2
+    assert arps_float.counts == [2/3, 2/3, 2/3]
 
 
 test_init()
