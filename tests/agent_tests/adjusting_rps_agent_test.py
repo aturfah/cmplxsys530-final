8c903db2368da7cd2ab5c8f1a188f7687e98d374
Ali Turfah <aturfah@umich.edu>
2018-10-30 09:41:43 -0400

    Add preliminary initialization tests

--- tests/agent_tests/adjusting_rps_agent_test.py
+++ tests/agent_tests/adjusting_rps_agent_test.py
@@ -1,11 +1,14 @@
 """Test for AdjustingRPSAgent."""
 
-from agent.adjusting_rps_agent import RPSAgent
+from agent.adjusting_rps_agent import AdjustingRPSAgent
 
 
 def test_init():
     """Test Initialization method."""
-    pass
+    arps = AdjustingRPSAgent()
+    assert arps.weight == 3
+    assert arps.original_strategy == arps.strategy
+    assert arps.counts == [1, 1, 1]
 
 
 test_init()
