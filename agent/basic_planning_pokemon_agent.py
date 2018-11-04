7d937ffd2266d6d7887699003a75d595ceb44a30
Ali Turfah <aturfah@umich.edu>
2018-11-04 08:16:10 -0500

    Add consideration of paralysis speed drop

--- agent/basic_planning_pokemon_agent.py
+++ agent/basic_planning_pokemon_agent.py
@@ -6,6 +6,8 @@ from copy import deepcopy
 from agent.basic_pokemon_agent import PokemonAgent
 from agent.basic_pokemon_agent import calc_opp_position_helper, calc_position_helper
 from config import USAGE_STATS, POKEMON_DATA, MOVE_DATA
+from config import (PAR_STATUS, FRZ_STATUS, SLP_STATUS,
+                    BRN_STATUS, PSN_STATUS, TOX_STATUS)
 
 
 class BasicPlanningPokemonAgent(PokemonAgent):
@@ -310,6 +312,14 @@ class BasicPlanningPokemonAgent(PokemonAgent):
         if p_move["priority"] == o_move["priority"]:
             speed_pairs = self.game_state.opp_gamestate["investment"][o_poke_name]["spe"]
             min_opp_spe, max_opp_spe = speed_pairs
+
+            # Factor in status
+            player_modifier = 1
+            opp_modifier = 1
+            if p_poke.status == PAR_STATUS:
+                player_modifier = player_modifier * 0.5
+            if opp_gs["data"]["active"]["status"] == PAR_STATUS:
+                opp_modifier = opp_modifier * 0.5
 
             # Assume that any speed is possible, which isn't exactly correct
             return p_poke.speed > (min_opp_spe + max_opp_spe) / 2
