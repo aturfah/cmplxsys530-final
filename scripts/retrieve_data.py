5f751dcb679116e1b8406220e644843a655c0767
Ali Turfah <aturfah@umich.edu>
2018-11-19 10:27:25 -0500

    Preliminary URL base

--- scripts/retrieve_data.py
+++ scripts/retrieve_data.py
@@ -1,13 +1,35 @@
 """ Script to pull in gen7pu data """
 import requests
+from datetime import datetime, timedelta
+
+
+def get_url_base(lag=1):
+    """
+    Get the month to use for the data.
+
+    Args:
+        lag (int): Number of months to push back.
+
+    Returns:
+        Month <lag> months behind in YYYY-MM format.
+
+    """
+    current_time = datetime.today().replace(day=1)
+    current_time = current_time - timedelta(months=lag)
+    time_str = current_time.strftime("%Y-%m")
+    url_base = "http://www.smogon.com/stats/{month}/chaos".format(month=time_str)
+    print(requests.get(url_base))
+    return url_base
 
 
 if __name__ == "__main__":
     print("Starting Data Retreival")
     TIERS = ["ou", "uu", "ru", "nu", "pu"]
-    BASE_URL = "http://www.smogon.com/stats/2017-12/chaos/gen7{tier}-{level}.json"
+    BASE_URL = "{url_base}/gen7{tier}-{level}.json".format(url_base=get_url_base())
     LEVELS = [0, 1500, 1630, 1760]
     OU_LEVELS = [0, 1500, 1695, 1825]
+
+    raise RuntimeError("DOOT")
 
     for tier in TIERS:
         for ind, level in enumerate(LEVELS):
