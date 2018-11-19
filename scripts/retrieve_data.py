96ce2f0ab1ddcd870f18ae07413c86e0ed1d2b8f
Ali Turfah <aturfah@umich.edu>
2018-11-19 09:44:51 -0500

    Preliminary script refactoring

--- scripts/retrieve_data.py
+++ scripts/retrieve_data.py
@@ -1,17 +1,20 @@
 """ Script to pull in gen7pu data """
 import requests
-import re
 
 
-print("Here")
-BASE_URL = "http://www.smogon.com/stats/2017-12/chaos/gen7pu-{}.json"
-LEVELS = [0, 1500, 1630, 1760]
+if __name__ == "__main__":
+    print("Starting Data Retreival")
+    TIERS = ["ou", "uu", "ru", "nu", "pu"]
+    BASE_URL = "http://www.smogon.com/stats/2017-12/chaos/gen7{tier}-{level}.json"
+    LEVELS = [0, 1500, 1630, 1760]
 
-for level in LEVELS:
-    print(level)
-    raw_html = requests.get(BASE_URL.format(level)).content
-    file_ = open("raw_data/gen7pu-{}.json".format(level), mode='wb')
-    file_.write(raw_html)
-    file_.close()
+    for tier in TIERS:
+        for level in LEVELS:
+            print("\t{}-{}".format(tier, level))
+            raw_html = requests.get(BASE_URL.format(tier=tier, level=level)).content
+            file_ = open("raw_data/gen7{tier}-{level}.json".format(tier=tier,
+                                                                   level=level), mode='wb')
+            file_.write(raw_html)
+            file_.close()
 
-print("Done")
+    print("Completed")
