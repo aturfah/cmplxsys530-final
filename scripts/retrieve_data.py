dc627be3db6ee63e398038657b83a9c0fa120578
Ali Turfah <aturfah@umich.edu>
2018-11-19 10:08:57 -0500

    Handle OU using different cutoffs

--- scripts/retrieve_data.py
+++ scripts/retrieve_data.py
@@ -7,9 +7,13 @@ if __name__ == "__main__":
     TIERS = ["ou", "uu", "ru", "nu", "pu"]
     BASE_URL = "http://www.smogon.com/stats/2017-12/chaos/gen7{tier}-{level}.json"
     LEVELS = [0, 1500, 1630, 1760]
+    OU_LEVELS = [0, 1500, 1695, 1825]
 
     for tier in TIERS:
-        for level in LEVELS:
+        for ind, level in enumerate(LEVELS):
+            if tier == "ou":
+                level = OU_LEVELS[ind]
+
             print("\tFetching {}-{}".format(tier, level))
             raw_html = requests.get(BASE_URL.format(tier=tier, level=level)).content
             file_ = open("data/usage/gen7{tier}-{level}.json".format(tier=tier,
