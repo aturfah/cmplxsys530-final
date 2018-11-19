5a6529156050a3e507e2ad3216e41b79a0587988
Ali Turfah <aturfah@umich.edu>
2018-11-19 09:49:58 -0500

    Update message (again)

--- scripts/retrieve_data.py
+++ scripts/retrieve_data.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
 
     for tier in TIERS:
         for level in LEVELS:
-            print("\tTier:{} | Level:{}".format(tier, level))
+            print("\tFetching {}-{}".format(tier, level))
             raw_html = requests.get(BASE_URL.format(tier=tier, level=level)).content
             file_ = open("data/usage/gen7{tier}-{level}.json".format(tier=tier,
                                                                      level=level), mode='wb')
