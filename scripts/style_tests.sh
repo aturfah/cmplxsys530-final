60d15409d43d8645ffc131d1ad213904c5519ff9
Ali Turfah <aturfah@gmail.com>
2019-01-27 22:36:06 -0500

    Add preliminary style tests script

--- scripts/style_tests.sh
+++ scripts/style_tests.sh
@@ -1,3 +1,13 @@
 # Script to run code style tests
 
-echo "Hello World!"
+
+if [ "'$*'" != "''" ]; then
+    # If arguments provided, use them
+    while [ "$1" != "" ]; do
+        echo "Received: ${1}" && shift;
+    done;
+else 
+    # Default to test all modules
+    MODULES=("agent" "battle_engine" "interface" "ladder" "file_manager" "pokemon_helpers" "scripts" "simulation" "stats" "tests")
+    ./scripts/style_tests.sh  "${MODULES[@]}"
+fi;
