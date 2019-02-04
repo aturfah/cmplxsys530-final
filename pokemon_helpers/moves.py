7d7778d266eb6fa10093ddc7900e15b390d314af
Ali Turfah <aturfah@gmail.com>
2019-02-03 20:45:18 -0500

    Add preliminary BaseMove

---
+++ pokemon_helpers/moves.py
@@ -0,0 +1,13 @@
+"""Classes defining Pokemon moves."""
+
+
+class BaseMove:
+    """Base class for all moves."""
+    def __init__(self):
+        """
+        Initialize a move.
+
+        Arguments defined by subclasses.
+        """
+        print("HELLO!")
+        self.name = "Jeff"
