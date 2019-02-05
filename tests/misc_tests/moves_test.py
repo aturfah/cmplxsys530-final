c01a7b63efbd2849a8f792cce53f2dd5e27ac490
Ali Turfah <aturfah@gmail.com>
2019-02-04 19:19:48 -0500

    Tests for calc_modifier and calc_damage

--- tests/misc_tests/moves_test.py
+++ tests/misc_tests/moves_test.py
@@ -1,7 +1,9 @@
 """Test script for Moves."""
 
 from pokemon_helpers.moves import BaseMove
+from pokemon_helpers.pokemon import Pokemon
 
+from config import MOVE_DATA
 
 def test_base_init():
     """Test the initialization of a BaseMove class."""
@@ -22,6 +24,28 @@ def test_brakcet_op():
 
 def test_calculate_damage():
     """Test that damage is calculated properly."""
+    tackle = BaseMove(**MOVE_DATA["tackle"])
+    exploud = Pokemon(name="exploud", moves=["return"])
+    floatzel = Pokemon(name="floatzel", moves=["shadowball"])
+
+    damage, _ = tackle.calculate_damage(exploud, floatzel, True)
+    assert damage == 78
+
+
+def test_calculate_modifier():
+    """Test that modifier is calculated properly."""
+    tackle = BaseMove(**MOVE_DATA["tackle"])
+    exploud = Pokemon(name="exploud", moves=["return"])
+    floatzel = Pokemon(name="floatzel", moves=["shadowball"])
+    gengar = Pokemon(name="gengar", moves=["tackle"])
+    regirock = Pokemon(name="regirock", moves=["selfdestruct"])
+
+    assert tackle.calculate_modifier(exploud, gengar) == 0  # No Effect
+    assert tackle.calculate_modifier(exploud, floatzel) == 1.5 # STAB
+    assert tackle.calculate_modifier(floatzel, regirock) == 0.5 # Not Very Effective
+    assert tackle.calculate_modifier(exploud, regirock) == 0.75 # Multiply together
 
 test_base_init()
 test_brakcet_op()
+test_calculate_damage()
+test_calculate_modifier()
